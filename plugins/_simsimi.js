63a7102c4e285ad6bdac43fd5fcb377794f74f82
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-12 08:08:26 -0500

    Actualizar _simsimi.js

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -8,12 +8,11 @@ handler.before = async (m) => {
   if (chat.simi) {
     if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
     let textodem = m.text;
-    //m.react('🗣️')
     try {
       const ressimi = await simitalk(textodem);
       await m.conn.sendMessage(m.chat, { text: ressimi.resultado.simsimi }, { quoted: fkontak });
     } catch {
-      throw '⚙️ La API de Simsimi presenta errores.';
+      throw '✧ Ocurrió un error inesperado.';
     }
     return !0;
   }
@@ -22,18 +21,18 @@ handler.before = async (m) => {
 export default handler;
 
 async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "es") {
-    if (!ask) return { status: false, resultado: { msg: "Debes ingresar un texto para hablar con simsimi." }};
+    if (!ask) return { status: false, resultado: { msg: "✧ Debes ingresar un texto para hablar con SimSimi." }};
     try {
         const response1 = await axios.get(`https://delirios-api-delta.vercel.app/tools/simi?text=${encodeURIComponent(ask)}`);
         const trad1 = await translate(`${response1.data.data.message}`, {to: language, autoCorrect: true});
-        if (trad1.text == 'indefinida' || response1 == '' || !response1.data) trad1 = XD // Se usa "XD" para causar error y usar otra opción.  
+        if (trad1.text == 'indefinida' || response1 == '' || !response1.data) trad1 = XD 
         return { status: true, resultado: { simsimi: trad1.text }};        
     } catch {
         try {
             const response2 = await axios.get(`https://anbusec.xyz/api/v1/simitalk?apikey=${apikeyyy}&ask=${ask}&lc=${language}`);
             return { status: true, resultado: { simsimi: response2.data.message }};       
         } catch (error2) {
-            return { status: false, resultado: { msg: "Todas las API's fallarón. Inténtalo de nuevo más tarde.", error: error2.message }};
+            return { status: false, resultado: { msg: "✧ Todas las Api's se cayeron, prueba a intentar más tarde.", error: error2.message }};
         }
     }
 }
