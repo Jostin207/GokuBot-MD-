9773b701baa4434aaaa0d45613d23e03f6c37b2b
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-12 16:50:42 -0500

    Actualizar bot-staff.js

--- plugins/bot-staff.js
+++ plugins/bot-staff.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let st = `https://telegra.ph/file/c366c44e1efae9417a46d.jpg`
 let staff = `💫 *EQUIPO STAFF*
 🥷 *Bot:* ${global.botname}
 ☁️ *Versión:* ${global.vs}
@@ -43,7 +42,7 @@ let staff = `💫 *EQUIPO STAFF*
 ✨️ *Número:* Wa.me/595983799436
 🏆 *GitHub:* https://github.com/DanixlJs`
 
-await conn.sendFile(m.chat, '${st}', 'yoshiko.jpg', staff.trim(),
+await conn.sendFile(m.chat, 'https://telegra.ph/file/c366c44e1efae9417a46d.jpg', 'yoshiko.jpg', staff.trim(),
 fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
