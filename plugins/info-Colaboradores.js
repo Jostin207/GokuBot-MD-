b59ec0a1329e4d694a52a3f790820327476a5a9d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-12 13:47:16 -0500

    Actualizar info-Colaboradores.js

--- plugins/info-Colaboradores.js
+++ plugins/info-Colaboradores.js
@@ -43,7 +43,21 @@ let staff = `🏆 *EQUIPO DE AYUDANTES*
 ✨️ *Número:* Wa.me/595983799436
 🏆 *GitHub:* https://github.com/DanixlJs`
 
-await conn.sendFile(m.chat, pp, 'yoshiko.mp4', staff, fake, m)}
+await conn.sendFile(m.chat, pp, 'yoshiko.mp4', staff, fake,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: `🤖 𝐈𝐍𝐅𝐈𝐍𝐈𝐓𝐘𝐖𝐀-𝐇𝐎𝐒𝐓 🤖`,
+body: `✅ Hosting de Calidad`,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://qu.ax/EQTd.jpg', 
+sourceUrl: 'https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A'}}},
+//{ quoted: fkontak2})
+})}
+
 
 handler.command = /^(staff|colabs|colaboradores)$/i
 export default handler
