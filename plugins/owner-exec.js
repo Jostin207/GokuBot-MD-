a6ef58fb1387bd059e6ba9e6867ae46099e52c38
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-13 12:40:18 -0400

    Actualizar owner-exec.js

--- plugins/owner-exec.js
+++ plugins/owner-exec.js
@@ -13,7 +13,7 @@ const handler = async (m, _2, msg, pickRandom, isOwner ) => {
   const name = conn.getName(m.sender);
   let _syntax = '';
   const _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix;
-  const old = m.experiencia * 1;
+  const old = m.exp * 1;
   try {
     let i = 15;
     const f = {exports: {}};
@@ -29,11 +29,11 @@ const handler = async (m, _2, msg, pickRandom, isOwner ) => {
     _return = e;
   } finally {
     conn.reply(m.chat, _syntax + format(_return), m);
-    m.experiencia = old;
+    m.exp = old;
   }
 };
 
-handler.help = ['> ', '=> '];
+handler.help = ['>', '=>'];
 handler.rowner = true,
 handler.tags = ['advanced'];
 handler.customPrefix = /=?>|~/;
