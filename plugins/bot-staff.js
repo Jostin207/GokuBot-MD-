ea5ad6c5eb62511a2942497b95571fe8048f9e39
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-12 21:05:16 -0500

    Actualizar bot-staff.js

--- plugins/bot-staff.js
+++ plugins/bot-staff.js
@@ -1,5 +1,6 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let user = global.db.data.users[m.sender]
+const role = args.slice(1).join(' ');
 let staff = `💫 *EQUIPO STAFF*
 🥷 *Bot:* ${global.botname}
 🪴 *Ofc Bot:* Wa.me/${ofcbot}
@@ -8,39 +9,39 @@ let staff = `💫 *EQUIPO STAFF*
 👑 *Propietario:*
 
 • Diego-YL-177
-☘️ *Rol:* ${rango}
+☘️ *Rol:* ${role}
 ✨️ *Número:* ${creador}
 🏆 *GitHub:* https://github.com/Diego-YL-177
 
 💫 *Colaboradores:*
 
 • GataNina-Li
-☘️ *Rol:* ${rango}
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/593968263524
 🏆 *GitHub:* https://github.com/GataNina-Li
 
 • elrebelde21
-☘️ *Rol:* ${rango}
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/573147616444
 🏆 *GitHub:* https://github.com/elrebelde21
 
 • KatashiFukushima
-☘️ *Rol:* ${rango}
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/51948705559
 🏆 *GitHub:* https://github.com/KatashiFukushima
 
 • AzamiJs
-☘️ *Rol:* ${rango}
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/5214434703586
 🏆 *GitHub:* https://github.com/AzamiJs
 
 • Alba070503
-☘️ *Rol:* ${rango}
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/59169082575
 🏆 *GitHub:* https://github.com/Alba070503
 
 • Danixl.Js
-☘️ *Rol:* ${rango}
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/595983799436
 🏆 *GitHub:* https://github.com/DanixlJs`
 
