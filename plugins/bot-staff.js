df0a6fa109678f903fc3af403a8f2f8c6b7a6c04
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-12 20:10:40 -0500

    Actualizar bot-staff.js

--- plugins/bot-staff.js
+++ plugins/bot-staff.js
@@ -6,39 +6,39 @@ let staff = `💫 *EQUIPO STAFF*
 👑 *Propietario:*
 
 • Diego-YL-177
-☘️ *Rol:* Propietario
+☘️ *Rol:* ${role}
 ✨️ *Número:* ${creador}
 🏆 *GitHub:* https://github.com/Diego-YL-177
 
 💫 *Colaboradores:*
 
 • GataNina-Li
-☘️ *Rol:* Developer
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/593968263524
 🏆 *GitHub:* https://github.com/GataNina-Li
 
 • elrebelde21
-☘️ *Rol:* Developer
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/573147616444
 🏆 *GitHub:* https://github.com/elrebelde21
 
 • KatashiFukushima
-☘️ *Rol:* Developer
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/51948705559
 🏆 *GitHub:* https://github.com/KatashiFukushima
 
 • AzamiJs
-☘️ *Rol:* Developer
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/5214434703586
 🏆 *GitHub:* https://github.com/AzamiJs
 
 • Alba070503
-☘️ *Rol:* Developer
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/59169082575
 🏆 *GitHub:* https://github.com/Alba070503
 
 • Danixl.Js
-☘️ *Rol:* Developer
+☘️ *Rol:* ${role}
 ✨️ *Número:* Wa.me/595983799436
 🏆 *GitHub:* https://github.com/DanixlJs`
 
