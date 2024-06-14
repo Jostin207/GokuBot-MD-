bd6de4bde8e303060a01ce7fbf27571c538ee371
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-14 08:33:20 -0500

    Actualizar owner-chatgp.js

--- plugins/owner-chatgp.js
+++ plugins/owner-chatgp.js
@@ -18,7 +18,7 @@ const handler = async (m, {conn, text, groupMetadata}) => {
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   const name = await conn.getName(m.sender);
   const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map((v) => v[0]);
-    const fakegif = {key: {participant: `0@s.whatsapp.net`, ...('6289643739077-1613049930@g.us' ? {remoteJid: '6289643739077-1613049930@g.us'} : {})}, message: {'videoMessage': {'title': '🧿⸽⃕Yᴏᴛsᴜʙᴀ - Nᴀᴋᴀɴᴏ - MD🍁⃨፝⃕✰', 'h': `Hmm`, 'seconds': '99999', 'gifPlayback': 'true', 'caption': '🧿 𝚈𝚘𝚝𝚜𝚞𝚋𝚊 - 𝙽𝚊𝚔𝚊𝚗𝚘 🌸', 'jpegThumbnail': false}}};
+    const fakegif = {key: {participant: `0@s.whatsapp.net`, ...('6289643739077-1613049930@g.us' ? {remoteJid: '6289643739077-1613049930@g.us'} : {})}, message: {'videoMessage': {'title': '💖⸽⃕Yᴏsʜɪᴋᴏ Bᴏᴛ - MD🍁⃨፝⃕✰', 'h': `Hmm`, 'seconds': '99999', 'gifPlayback': 'true', 'caption': '💖 𝚈𝚘𝚜𝚑𝚒𝚔𝚘 𝙱𝚘𝚝 - 𝙼𝙳 🌹', 'jpegThumbnail': false}}};
   const teks = `*🧿 • 𝙶𝚛𝚞𝚙𝚘:* ${groupMetadata.subject}\n*🌸 • 𝙳𝚎:* ${name}\n*🍁 • 𝙽𝚞́𝚖𝚎𝚛𝚘:* wa.me/${who.split`@`[0]}\n*📧 • 𝙼𝚎𝚗𝚜𝚊𝚓𝚎:* ${text}`;
   for (const id of groups) {
     await conn.sendMessage(id, {text: teks}, {quoted: fakegif});
