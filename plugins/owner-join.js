9c0e4df88866e2cdfcd030a2c3f1c5330af9f964
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-14 14:55:35 -0500

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -4,15 +4,14 @@ import fs from 'fs'
 import { execSync } from 'child_process'
 import { spawn } from 'child_process'
 import chalk from "chalk" 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, command, usedPrefix, text, isAdmin, isOwner, isROwner, participants, groupMetadata  }) => {
 
 const isCommand1 = /^(join|nuevogrupo|newgrupo|unete)$/i.test(command)
 
 async function reportError(e) {
-await m.reply(`🍃 Error`)
-console.log(`🍃 Error`)
+await m.reply(`🥷 Error`)
+console.log(`🥷 Error`)
 console.log(e)
 }
 
@@ -21,12 +20,12 @@ case isCommand1:
 let user = m.sender.split('@')[0] 
 let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
 let [_1, code1] = link.match(linkRegex) || []
-if (!code1) return m.reply(lenguajeYL.smsJoin1(usedPrefix, command))
+if (!code1) return m.reply(`╰⊱🌩⊱ 𝗘𝗥𝗥𝗢𝗥 ⊱🌩⊱╮\n\n🍓 *𝙸𝚗𝚐𝚛𝚎𝚜𝚎 𝙴𝚕 𝙴𝚗𝚕𝚊𝚌𝚎 𝙳𝚎 𝙰𝚕𝚐𝚞𝚗 𝙶𝚛𝚞𝚙𝚘*\n*𝙴𝚓𝚎𝚖𝚙𝚕𝚘:*\n*${usedPrefix + command}* ${gp2}`)
 try {      
 if ( isOwner || m.fromMe) {
-await m.reply(lenguajeYL.smsJoin2())
+await m.reply(`╰⊱🌺⊱ 𝗥𝗘𝗔𝗟𝗜𝗭𝗔𝗗𝗢 ⊱🌺⊱╮\n\n${packname}\n*𝚈𝚊 𝚂𝚎 𝙷𝚊 𝚄𝚗𝚒𝚍𝚘 𝙰𝚕 𝙶𝚛𝚞𝚙𝚘 🍒*`)
 let res1 = await conn.groupAcceptInvite(code1)
-await conn.sendMessage(res1, { text: lenguajeYL.smsJoin(user), mentions: (await conn.groupMetadata(`${res1}`)).participants.map(v => v.id) }, [user], { quoted: fkontak })
+await conn.sendMessage(res1, { text: `${packname}\n_🍬 𝚂𝚎 𝙷𝚊 𝚄𝚗𝚒𝚍𝚘 𝙰𝚕 𝙶𝚛𝚞𝚙𝚘_\n\n🧃 *𝙵𝚞𝚎 𝙰𝚐𝚛𝚎𝚐𝚊𝚍𝚊 𝙿𝚘𝚛: @${user}`(user), mentions: (await conn.groupMetadata(`${res1}`)).participants.map(v => v.id) }, [user], { quoted: fkontak })
 }} catch (e) {
 reportError(e)
 }        
