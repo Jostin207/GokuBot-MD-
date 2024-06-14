71a24753604bbb52e93299c1cb513b34186861a9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-14 13:49:10 -0500

    Actualizar koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
 sudo dnf clean all -y
 
-RUN git clone https://github.com/Diego-YL-177/YoshikoBot-MD
+RUN git clone https://github.com/Diego-YL-177/YaeromiBot-MD
 
-WORKDIR /root/YoshikoBot-MD
+WORKDIR /root/YaeromiBot-MD
 
-COPY ./root/YoshikoBot-MD
+COPY ./root/YaeromiBot-MD
 
 RUN yarn install
 
