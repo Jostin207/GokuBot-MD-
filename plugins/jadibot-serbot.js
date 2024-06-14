81e741e6f407f041a6cd9b2324ac405d50f0cd7f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-14 14:48:46 -0500

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -40,8 +40,8 @@ let crm3 = 'SBpbmZvLWRvbmFyLmpz'
 let crm4 = 'IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz'
 let drm1 = ''
 let drm2 = ''
-let rtx = lenguajeYL['smsIniJadi']() 
-let rtx2 = lenguajeYL['smsIniJadi2']() 
+let rtx = '𝗦𝗲𝗿 𝗝𝗮𝗱𝗶𝗯𝗼𝘁 𝗬𝗮𝗲𝗺𝗼𝗿𝗶𝗕𝗼𝘁 - 𝗠𝗗 🍒\n\n🚀 *Escanea Este Codigo QR Para Convertirte En Un Sub Bot Temporal.*\n\n🎌 *Pasos Para Escanear:*\n\n1. Diríjase a los tres puntos ubicado en la esquina superior derecha.\n\n2. Ir a la opción dispositivos vinculados.\n\n3. Da Click En Vincular Dispositivo & escanea este codigo qr.\n\n📢 *Aviso:* Este Código QR Expira En 45 Segundos.\n\n𝗕𝘆: 𝗬𝗮𝗲𝗺𝗼𝗿𝗶𝗕𝗼𝘁-𝗠𝗗 🍓'
+let rtx2 = '𝗦𝗲𝗿 𝗝𝗮𝗱𝗶𝗯𝗼𝘁 𝗬𝗮𝗲𝗺𝗼𝗿𝗶𝗕𝗼𝘁 - 𝗠𝗗 🍒\n\n🚀 *Utilice Este Codigo Para Convertirte En Un Sub Bot Temporal.*\n\n🎌 *Pasos Para Vincular:*\n\n1. Diríjase a los tres punto en la esquina superior derecha.\n\n2. Ir a la opción dispositivos vinculados.\n\n3. Da click en vincular con codigo de telefono & pega el código a continuación.\n\n𝗕𝘆:  𝗬𝗮𝗲𝗺𝗼𝗿𝗶𝗕𝗼𝘁-𝗠𝗗  🍓'
 
 if (global.conns instanceof Array) console.log()
 else global.conns = []
