e657df2ec5f9505633757c5c9e86f12a0d3c0638
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-15 08:57:41 -0500

    Delete plugins directory

--- plugins/herramientas-qrcode.js
+++
@@ -1,9 +0,0 @@
-import {toDataURL} from 'qrcode';
-const handler = async (m, {text, conn}) => {
-  if (!text) throw `*Y EL TEXTO?*`;
-  conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), {scale: 8}), 'qrcode.png', '¯\\_(ツ)_/¯', m);
-};
-handler.help = ['', 'code'].map((v) => 'qr' + v + ' <teks>');
-handler.tags = ['tools'];
-handler.command = /^qr(code)?$/i;
-export default handler;
