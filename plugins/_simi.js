ac93508ce88496fbc4b8ed1d650cf4f2c616faf6
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-16 21:58:31 -0500

    Actualizar plugins

---
+++ plugins/_simi.js
@@ -0,0 +1,37 @@
+import translate from '@vitalets/google-translate-api';
+import axios from 'axios';
+import fetch from 'node-fetch';
+const handler = (m) => m;
+
+handler.before = async (m) => {
+  if (m.sender === conn.user.jid) return;
+
+  const chat = global.db.data.chats[m.chat];
+  if (chat.simi) {
+   try {
+    let textodem = m.text;
+      const ressimi = await simitalk(textodem);
+      await m.conn.sendMessage(m.chat, { text: ressimi.resultado.simsimi }, { quoted: m });
+  } catch (err) {
+console.log("💭 Error", err)}
+}
+};
+
+export default handler;
+
+async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "es") {
+    if (!ask) return { status: false, resultado: { msg: "🥷 Debes ingresar un texto para hablar con SimSimi." }};
+    try {
+        const response1 = await axios.get(`https://delirios-api-delta.vercel.app/tools/simi?text=${encodeURIComponent(ask)}`);
+        const trad1 = await translate(`${response1.data.data.message}`, {to: language, autoCorrect: true});
+        if (trad1.text == 'indefinida' || response1 == '' || !response1.data) trad1 = XD 
+        return { status: true, resultado: { simsimi: trad1.text }};        
+    } catch {
+        try {
+            const response2 = await axios.get(`https://anbusec.xyz/api/v1/simitalk?apikey=${apikeyyy}&ask=${ask}&lc=${language}`);
+            return { status: true, resultado: { simsimi: response2.data.message }};       
+        } catch (error2) {
+            return { status: false, resultado: { msg: "🏓 Todas las Api's se cayeron, prueba a intentar más tarde.", error: error2.message }};
+        }
+    }
+}
