4fb3aa258cfb413e18f20b7ca037347291e76dd8
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 17:36:55 -0500

    Actualizar plugins

---
+++ plugins/downloader-ig.js
@@ -0,0 +1,18 @@
+import Scraper from '@SumiFX/Scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply('💥 Ingresa un enlace de Instagram.')
+try {
+let { dl_url } = await Scraper.igdl(args[0])
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `✅️ *Su Video De Instagram*\n${botname}` }, { quoted: fkontak})
+} catch (e) {
+  console.log(e)
+  m.reply('☘️ Ocurrió un error inesperado.')
+}}
+
+handler.help = ['ig <enlace>']
+handler.tags = ['downloader']
+handler.command = ['ig', 'instagram']
+handler.registrado = true 
+handler.diamantes = 1
+
+export default handler
