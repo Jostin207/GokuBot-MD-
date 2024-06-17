37ad75859d672970b214e7ca74d74b7b75bafb09
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 17:48:57 -0500

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -46,8 +46,11 @@ if (global.conns instanceof Array) console.log()
 else global.conns = []
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-let parentw = conn
-if (conn.user.jid !== global.conn.user.jid) return parentw.reply(m.chat, `「💭」Solo puedes usar este comando en el bot principal.\n\n• Wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix + command}`, fkontak) 
+if (!global.db.data.settings[conn.user.jid].modejadibot) {
+m.reply('👑 Este Comando está deshabilitado por mi creador.')
+return
+}
+return parentw.reply(m.chat, `「💭」Solo puedes usar este comando en el bot principal.\n\n• Wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix + command}`, fkontak) 
 const mcode = args[0] && args[0].includes('--code') ? true : args[1] && args[1].includes('--code') ? true : false
 
 
