3896533e72fafd73d21533bba45940852a54552e
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-16 22:06:19 -0500

    Actualizar bot-audio(a).js

--- plugins/bot-audio(a).js
+++ plugins/bot-audio(a).js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios && !m.isGroup) return;
   //const s = seconds: '1934.4'
-  const vn = './media/a.mp3';
+  const vn = 'https://raw.githubusercontent.com/GataNina-Li/GataBot-MD/media/a.mp4';
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3`}, {quoted: fkontak});
 };
