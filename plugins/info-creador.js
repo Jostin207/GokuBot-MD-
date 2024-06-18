fd4791d90b3820d9d1334b5809fdb4895f5cca9c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 22:15:25 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,13 +1,13 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = yoshiImg        
-const bot = `👑 𝑶𝑭𝑪 𝑫𝑰𝑬𝑮𝑶
+const bot = `👑 𝑂𝐹𝐶 𝐷𝐼𝐸𝐺𝑂
 *${creador}*
 ༺═──────────────═༻
 🥷 𝐵𝑂𝑇 𝑂𝐹𝐶
 *Wa.me/${ofcbot}*
 *༺═──────────────═༻*
-💌 𝑴𝑰 𝑪𝑶𝑹𝑹𝑬𝑶
+💌 𝑀𝐼 𝐶𝑂𝑅𝑅𝐸𝑂
 *theyoshikobot@gmail.com*
 ༺═──────────────═༻
 👨‍💻 𝐴𝑆𝐼𝑆𝑇𝐸𝑁𝐶𝐼𝐴
