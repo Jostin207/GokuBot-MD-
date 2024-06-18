d302bfb2d32c9721b89299b16f335f9441d78a08
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-18 13:17:25 -0500

    Actualizar plugins

---
+++ plugins/bot-ia.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';
+import axios from 'axios';
+import translate from '@vitalets/google-translate-api';
+import {Configuration, OpenAIApi} from 'openai';
+const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
+const openaiii = new OpenAIApi(configuration);
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (usedPrefix == 'a' || usedPrefix == 'A') return;
+if (!text) return conn.reply(m.chat, `💭 Ingrese una petición para que la IA lo responda, Ejemplo:\n> *${usedPrefix + command} Código de una Calculadora en Js*`, m, fake, )   
+try {
+m.react('⌛️')
+conn.sendPresenceUpdate('composing', m.chat);
+let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
+let res = await gpt.json()
+await m.reply(res.gpt)
+m.react('✅️')
+} catch {
+try {
+let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
+let res = await gpt.json()
+await m.reply(res.data) 
+m.react('✅️')
+} catch{
+}}}
+handler.help = ['chatgpt <texto>', 'ia <texto>']
+handler.tags = ['ai']
+handler. = true
+handler.limit = 2
+handler.command = ['ia', 'chatgpt']
+
+export default handler;
