e7404c17f2b0c117652a9191802acd037402276f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-18 13:53:27 -0500

    Actualizar bot-ia.js

--- plugins/bot-ia.js
+++ plugins/bot-ia.js
@@ -6,26 +6,21 @@ const configuration = new Configuration({organization: global.openai_org_id, api
 const openaiii = new OpenAIApi(configuration);
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 if (usedPrefix == 'a' || usedPrefix == 'A') return;
-if (!text) return conn.reply(m.chat, `🍃 *Ingrese una petición*\n\n- Ejemplo, !${command} Codigo en Js para un juego de ahorcado`, m, fake, )   
+if (!text) return conn.reply(m.chat, `🍃 *Ingrese una petición*\n\n- Ejemplo, !${command} Codigo en Js para un juego de ahorcado`, m, fake, )      
 try {
 await m.reply('🌺 *C A R G A N D O*\n- 🍃 Cargando Información')
-m.react('⌛️') 
+m.react('⌛')
 conn.sendPresenceUpdate('composing', m.chat);
 let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
 let res = await gpt.json()
 await m.reply(res.gpt)
-//m.react('✅️')
 } catch {
 try {
-m.react('✅️')
 let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
 let res = await gpt.json()
-await m.reply(res.data) 
-//m.react('✅️')
+await m.reply(res.data)
+m.react('✅')
 } catch {
 }}}
-handler.help = ['openia', 'chatgpt', 'ia']
-handler.tags = ['ai']
-handler.command = /^(openai|chatgpt|ia)$/i
-
+handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
 export default handler;
