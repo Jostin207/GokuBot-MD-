4c7a071e2c1e03d3d315fa0ba3ec680ec0b2cb19
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 21:37:34 -0500

    Actualizar contactos.js

--- plugins/contactos.js
+++ plugins/contactos.js
@@ -10,14 +10,14 @@ let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 let name = await conn.getName(who)
 
-  await sendContactArray(conn, m.chat, [
-     [`${nomorown}`, `👑 Propietario`, `🍂 𝗢𝗳𝗰𝗗𝗶𝗲𝗴𝗼`, team, 'argumedod44@gmail.com', `🇨🇴 Colombia`, `https://www.youtube.com/@Yoshiko_team`, bio],
-[`595983799436`, `𝗖𝗼𝗹𝗮𝗯𝗼𝗿𝗮𝗱𝗼𝗿 𝗘𝘀𝘁𝗿𝗲𝗹𝗹𝗮 🌟`, `🌟`, `𝗘𝘀𝘁𝗿𝗲𝗹𝗹𝗮 🌟`, 'devdanixljs00@gmail.com', `🇵🇾 Paraguay`, `https://github.com/DanixlJs`, `✨️ 𝗦𝗼𝘆 𝗢𝘄𝗻𝗲𝗿 𝗗𝗲 𝗬𝗼𝘀𝗵𝗶𝗸𝗼𝗕𝗼𝘁 👑`],
-[`59169082575`, `𝗗𝗲𝘀𝗮𝗿𝗿𝗼𝗹𝗹𝗮𝗱𝗼𝗿 𝗘𝘀𝘁𝗿𝗲𝗹𝗹𝗮 🌟`, `🌟`, `𝗗𝗲𝘀𝗮𝗿𝗿𝗼𝗹𝗹𝗮𝗱𝗼𝗿 😊`, 'alba070599@hotmail.com', `🇧🇴 Bolivia`, `https://github.com/Alba070503`, `Soy creador de ShizukaBot-MD y 🍁Rei Ayanami Bot MD 🍁`],
-['5215610314499', '𝗖𝗼𝗹𝗮𝗯𝗼𝗿𝗮𝗱𝗼𝗿𝗮 💖', '💖', '𝗖𝗼𝗹𝗮𝗯𝗼𝗿𝗮𝗱𝗼𝗿', 'thalk8980@gmail.com', '🇲🇽 Mexico', 'https://github.com/ArizzVal', 'Me gusta comer pene']
-], m)
-  //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mis creadores, no hagas spam!!`)
-  } 
+await sendContactArray(conn, m.chat, [
+[`${nomorown}`, '👑 Propietario', '🍂 𝗢𝗳𝗰𝗗𝗶𝗲𝗴𝗼', team, 'argumedod44@gmail.com', '🇨🇴 Colombia', 'https://www.youtube.com/@Yoshiko_team', bio],
+[`595983799436`, 'José Daniel', 'Danichu', '𝗦𝗼𝘆 𝗦𝘁𝗮𝗳𝗳 😊', 'devdanixljs00@gmail.com', '🇵🇾 Paraguay', 'https://github.com/DanixlJs', '✨️ 𝗦𝗼𝘆 𝗢𝘄𝗻𝗲𝗿 𝗗𝗲 𝗬𝗼𝘀𝗵𝗶𝗸𝗼𝗕𝗼𝘁 👑'],
+[`59169082575`, 'Yhair', 'Alba070503', '𝗦𝗼𝘆 𝗦𝘁𝗮𝗳𝗳 😊', 'alba070599@hotmail.com', '🇧🇴 Bolivia', `https://github.com/Alba070503`, 'Soy creador de ShizukaBot-MD y 🍁Rei Ayanami Bot MD 🍁'],
+['5215610314499', 'Arumi', 'YooKitsune', '𝗦𝗼𝘆 𝗦𝘁𝗮𝗳𝗳 😊', 'thalk8980@gmail.com', '🇲🇽 Mexico', 'https://github.com/ArizzVal', 'Me gusta comer pene'],
+['51939041500', 'Llilmer', 'LlilmerTy', '𝗦𝗼𝘆 𝗦𝘁𝗮𝗳𝗳 😊', 'llilmerlinogarcia0@gmail.com', '🇵🇪 Perú', 'https://github.com/Llilmer-G-Shock', '✨️ 𝗦𝗼𝘆 𝗢𝘄𝗻𝗲𝗿 𝗗𝗲 𝗬𝗼𝘀𝗵𝗶𝗸𝗼𝗕𝗼𝘁 👑']
+[`${conn.user.jid.split('@')[0]}`, `Es Una Bot 🚀`, `${packname}`, `📵 No Hacer Spam`, 'theyoshikobot@gmail.com', `🇨🇴 Colombia`, `https://github.com/Diego-YL-177/YoshikoBot-MD`, biobot]
+], m)} 
 
 handler.help = ["contactos","contacto"]
 handler.tags = ["owner"]
@@ -25,14 +25,14 @@ handler.command = /^(contactos|contacto)$/i
 export default handler
 
 async function sendContactArray(conn, jid, data, quoted, options) {
-        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
-                let contacts = []
-        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
-            number = number.replace(/[^0-9]/g, '')
-            let njid = number + '@s.whatsapp.net'
-            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
-            // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
-            let vcard = `
+if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+let contacts = []
+for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+number = number.replace(/[^0-9]/g, '')
+let njid = number + '@s.whatsapp.net'
+let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+// N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
+let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
@@ -49,16 +49,16 @@ item4.URL:${isi4}
 item4.X-ABLabel:Website
 item5.X-ABLabel:${isi5}
 END:VCARD`.trim()
-            contacts.push({ vcard, displayName: name })
-        }
-        return await conn.sendMessage(jid, {
-            contacts: {
-                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
-                contacts,
-            }
-        },
-        {
-            quoted,
-            ...options
-        })
-        }
+contacts.push({ vcard, displayName: name })
+}
+return await conn.sendMessage(jid, {
+contacts: {
+displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+contacts,
+}
+},
+{
+quoted,
+...options
+})
+}
