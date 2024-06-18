e3fdbd05edd905ab0c2a080ce16be0dad76dee86
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-18 10:38:06 -0500

    Actualizar mods-ip.js

--- plugins/mods-ip.js
+++ plugins/mods-ip.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 
 let handler = async (m, { conn, text }) => {
 //await m.reply("🧑🏻‍💻 Buscando...")
+let bot = '🧑🏻‍💻 Buscando....'
+conn.reply(m.chat, bot, m, fake, )
   if (!text) return conn.reply(m.chat, "🥷 *Te Faltó La <Ip>*", m, fake, )
 
   axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`).then ((res) => {
