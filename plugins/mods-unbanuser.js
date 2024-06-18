72f1ca806d54ab5459730e0587bd510580cd6a91
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 20:38:08 -0500

    Actualizar plugins

---
+++ plugins/mods-unbanuser.js
@@ -0,0 +1,28 @@
+const handler = async (m, { conn, args, text, usedPrefix, command }) => {
+    let user;
+    let db = global.db.data.users;
+    if (m.quoted) {
+        user = m.quoted.sender;
+    } else if (args.length >= 1) {
+        user = args[0].replace('@', '') + '@s.whatsapp.net';
+    } else {
+        await conn.reply(m.chat, `💥 Etiqueta o responde al mensaje del usuario que quieras Desbanear, Ejemplo:\n> → *${usedPrefix}unbanuser <@tag>*`, m);
+        return;
+    }
+    if (db[user]) {
+        db[user].baneado = false;
+        db[user].banRazon = '';
+        const nametag = await conn.getName(user);
+        const nn = conn.getName(m.sender);
+        await conn.reply(m.chat, `✅️ El usuario *${nametag}* ha sido desbaneado.`, m, { mentionedJid: [user] });
+        conn.reply('120363284046748076@g.us', `💭 El usuario *${nametag}* ha sido desbaneado por *${nn}*`, m, fake, );
+    } else {
+        await conn.reply(m.chat, '💥 El usuario no está registrado.`, m);
+    }
+};
+handler.help = ['unbanuser <@tag>'];
+handler.command = ['unbanuser'];
+handler.tags = ['mods'];
+handler.mods = true;
+handler.group = true;
+export default handler;
