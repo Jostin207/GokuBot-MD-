9d81b1e9ae4d713bc0b778c2df03c06e5851d302
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 20:27:54 -0500

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('✧ Ya eras Admin del Grupo.');
+  if (isAdmin) return m.reply('☘️ Ya eras Admin del Grupo.');
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
     m.react('✅')
