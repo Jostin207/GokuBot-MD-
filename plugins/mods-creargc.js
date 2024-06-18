9d35012122f1aefe08782218d4125469c955ecc3
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-18 13:09:20 -0500

    Actualizar mods-creargc.js

--- plugins/mods-creargc.js
+++ plugins/mods-creargc.js
@@ -7,8 +7,8 @@ let link = await conn.groupInviteCode(group.gid)
 m.reply('https://chat.whatsapp.com/' + url)
 } catch (e) {
 m.reply(`✨️ Ocurrió un error inesperado.`)
-conn.reply('573012482597@s.whatsapp.net', `💭 El Moderador ${nombre} Ha Creado Un Grupo Con ${wm}`, m, fake, );
 }
+conn.reply('573012482597@s.whatsapp.net', `💭 El Moderador ${nombre} Ha Creado Un Grupo Con ${wm}`, m, fake, );
 }
 handler.help = ['grupocrear <nombre>']
 handler.tags = ['mods']
