382468e9ebeb5b32f908ddddac516718fad6f161
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-18 11:17:24 -0500

    Actualizar owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,24 +1,22 @@
-import cp, { exec as _exec } from 'child_process'
-import { promisify } from 'util'
 
-let exec = promisify(_exec).bind(cp)
-
-var handler = async (m, { conn, isOwner, command, text }) => {
-
-if (conn.user.jid != conn.user.jid) return
-
-let o
-try {
-o = await exec(command.trimStart()  + ' ' + text.trimEnd())
-} catch (e) {
-o = e
-} finally {
-let { stdout, stderr } = o
-if (stdout.trim()) conn.reply(m.chat, stdout, m, fake, )
-if (stderr.trim()) conn.reply(m.chat, stderr, m, fake, )
-}
-
-}
+import cp, {exec as _exec} from 'child_process';
+import {promisify} from 'util';
+const exec = promisify(_exec).bind(cp);
+const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROwner}) => {
+  if (!isROwner) return;
+  if (global.conn.user.jid != conn.user.jid) return;
+  m.reply('💥 *Ejecutando orden.*');
+  let o;
+  try {
+    o = await exec(command.trimStart() + ' ' + text.trimEnd());
+  } catch (e) {
+    o = e;
+  } finally {
+    const {stdout, stderr} = o;
+    if (stdout.trim()) m.reply(stdout);
+    if (stderr.trim()) m.reply(stderr);
+  }
+};
 handler.help = ['$']
 handler.tags = ['owner']
 handler.customPrefix = /^[$] /
