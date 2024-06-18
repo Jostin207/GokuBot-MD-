a58744177ea51a771e983ebf0f235b1123ce0e5b
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 20:24:31 -0500

    Actualizar owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,54 +1,34 @@
-var handler = async (m, { conn, text, usedPrefix, command}) => {
+const handler = async (m, { conn, args, text, usedPrefix, command, isROwner }) => {
+    let user;
+    let reason;
+    let db = global.db.data.users;
+    if (m.quoted) {
+        user = m.quoted.sender;
+        reason = args.join(' ');
+    } else if (args.length >= 2) {
+        user = args[0].replace('@', '') + '@s.whatsapp.net';
+        reason = args.slice(1).join(' ');
+    } else {
+        await conn.reply(m.chat, `🌟 Etiqueta al usuario que quieras Banear.`, m);
+        return;
+    }
+    if (user === isROwner) return m.reply('💫 No puedes banear a mi Creador.');
+    if (db[user]) {
+        db[user].baneado = true;
+        db[user].banRazon = `${reason}`;
+        const nametag = await conn.getName(user);
+        const nn = conn.getName(m.sender);
+        await conn.reply(m.chat, `💥 El usuario *${nametag}* ha sido Baneado.\n> ✰ *Razón ⪼* ${reason}`, m, { mentionedJid: [user] });
+        await conn.reply('120363284046748076@g.us', `💭 El usuario *${nametag}* ha sido Baneado por *${nn}*.\n> ✰ *Razón ⪼* ${reason}`, m, fake, );
+    } else {
+        m.reply(m.chat, `🥷 El usuario no está registrado.`);
+    }
+};
 
-let user, number, bot, bant, ownerNumber, aa, users, usr, q, mime, img
+handler.help = ['banuser <@tag> <razón>'];
+handler.command = ['banuser'];
+handler.tags = ['mods'];
+handler.mods = true;
+handler.group = true;
 
-try {
-function no(number){
-return number.replace(/\s/g,'').replace(/([@+-])/g,'')}
-text = no(text)
-if(isNaN(text)) {
-number = text.split`@`[1]
-} else if(!isNaN(text)) {
-number = text
-}
-user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
-bot = conn.user.jid.split`@`[0] 
-bant = `☄️ *Etiquete a una persona*\n\nEjemplo, !${command} @${global.suittag}`
-if (!text && !m.quoted) return conn.reply(m.chat, bant, m, { mentions: [user] })               
-try {
-if(text) {
-user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-user = m.quoted.sender
-} else if(m.mentionedJid) {
-user = number + '@s.whatsapp.net'
-}} catch (e) {
-} finally {
-number = user.split('@')[0]
-if(user === conn.user.jid) return conn.reply(m.chat, `☄️ @${bot} *No puede ser baneado con este comando*`, m, { mentions: [user] })   
-for (let i = 0; i < global.owner.length; i++) {
-ownerNumber = global.owner[i][0];
-if (user.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
-aa = ownerNumber + '@s.whatsapp.net'
-await conn.reply(m.chat, `☄️ *No puedo banear al propietario @${ownerNumber} de ${wm}*`, m, { mentions: [aa] })
-return
-}}
-users = global.db.data.users
-if (users[user].banned === true) conn.reply(m.chat, `☄️ *No es necesario volver a banear a @${number}*`, m, { mentions: [user] }) 
-users[user].banned = true
-usr = m.sender.split('@')[0]     
-await conn.reply(m.chat, '✅ *Usuario baneado con éxito*', m, { mentions: [user] })   
-
-}} catch (e) {
-await conn.reply(m.chat, '🛑 *Ocurrió un fallo*', m, fake, )
-console.log(e) 
-}
-
-}
-handler.tags = ['owner']
-handler.commnad = ['banuser']
-handler.command = /^banuser$/i
-
-handler.rowner = true
-
-export default handler
+export default handler;
