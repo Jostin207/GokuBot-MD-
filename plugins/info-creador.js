cd8242bf2bfdbe4f2a7f648b48fbe2435f08d69a
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-17 22:20:46 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,8 +1,8 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = yoshiImg   
-let bot = 'YoshikoBot-MD'     
-const bot = `*_☘️ Hola ${nombre}, Me llamo ${bot} Siempre a disposición para ayudarte ✨️_*
+let botsito = 'YoshikoBot-MD'     
+const bot = `*_☘️ Hola ${nombre}, Me llamo ${botsito} Siempre a disposición para ayudarte ✨️_*
 
 ${packname}
 *◎ ${creador}*
