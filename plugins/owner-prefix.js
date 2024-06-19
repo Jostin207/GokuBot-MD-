ccedfc61d2355a5cba3a0c3e483f57a7601c1a16
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-19 18:34:05 -0500

    Actualizar owner-prefix.js

--- plugins/owner-prefix.js
+++ plugins/owner-prefix.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*🍭 No Se Encontró Ningun Prefijo, Por Favor Escriba Un Prefijo. Ejemplo:* ${usedPrefix + command} /`;
+  if (!text) throw `*✨️ No Se Encontró Ningun Prefijo, Por Favor Escriba Un Prefijo. Ejemplo:* ${usedPrefix + command} !`;
   global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
  // await m.reply(`*✅️ Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`);
   conn.fakeReply(m.chat, `✅️ *Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`, '0@s.whatsapp.net', '💌 PREFIJO NUEVO 💌')
