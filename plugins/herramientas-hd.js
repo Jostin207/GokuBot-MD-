585e9564394a3c51f762e6b96c93c92f84b44408
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-19 08:52:55 -0500

    Actualizar herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   let pr = await remini(img, "enhance");
   conn.sendMessage(m.chat, {image: pr}, {quoted: fkontak});
  } catch {
-  throw "🛑 Ocurrió Un Error Inesperado";
+  throw "✨️ Ocurrió Un Error Inesperado 💌";
  }
 };
 handler.help = ["remini", "hd", "enhance"];
