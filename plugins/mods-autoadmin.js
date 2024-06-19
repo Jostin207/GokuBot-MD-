745b5e4ac2b9d9b154bfddb6f48bce2b71a13582
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-19 11:37:02 -0500

    Actualizar mods-autoadmin.js

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -1,9 +1,9 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('💌 ¡YA ERES ADM JEFE! 💌');
+  if (isAdmin) return m.reply('💌 *¡YA ERES ADM JEFE!* 💌');
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
     m.react('✅')
-   return m.reply('💌 ¡LISTO JEFE!');
+   return m.reply('👑 ¡YA TE DI ADM MI JEFE!*!');
     let nn = conn.getName(m.sender);
     conn.reply('573012482597@s.whatsapp.net', `💥 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, fake, );
   } catch {
