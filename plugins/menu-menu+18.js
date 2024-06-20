0803455e2da68bd272afba3ad23ef1bbae3502f6
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-19 21:39:37 -0500

    Actualizar menu-menu+18.js

--- plugins/menu-menu+18.js
+++ plugins/menu-menu+18.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `💥 *¡Estos comandos están desactivados!*`; 
   try {
-    const pp = 'https://qu.ax/hkkv.jpg';
+    const pp = './Menu.jpg';
    // const vn = 'https://qu.ax/Epen.mp3';
     const d = new Date(new Date + 3600000);
     const locale = 'es';
