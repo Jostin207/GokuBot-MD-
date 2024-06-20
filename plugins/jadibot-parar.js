ed7586ac364a96426775c23b011bb0ddcfdcf3ab
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-19 22:56:37 -0500

    Actualizar jadibot-parar.js

--- plugins/jadibot-parar.js
+++ plugins/jadibot-parar.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
-if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `👑 El Bot Principal No Se Puede Apagar`, m, fake, )
+if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `👑 El Bot Principal No Se Puede Apagar`, m, rcanal, )
 else {
-await conn.reply(m.chat, `🔴 Subbot Desactivado`, m, fake, )
+await conn.reply(m.chat, `🔴 Subbot Desactivado`, m, rcanal, )
 conn.ws.close()
 }}
 handler.command = handler.help = ['stop', 'byebot'];
