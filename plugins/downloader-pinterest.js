94fa0303eb52fe424afbe96365e2901065abd21f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-20 13:02:30 -0500

    Actualizar downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -4,7 +4,33 @@ if (!text) return m.reply('👑 Ingresa un texto para realizar la búsqueda.')
 try {
 m.react(rwait)
 let { dl_url } = await Scraper.pinterest(text)
-await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
+//await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
+    const selectedResults = results.slice(0, 7);
+
+    const push = await Promise.all(selectedResults.map(async (result) => ({
+        body: { text: null },
+        footer: { text: `💥 Ofc Diego` },
+        header: {
+            title: `${text}`,
+            hasMediaAttachment: true,
+            videoMessage: await createVideo(dl_url)
+        },
+        nativeFlowMessage: { buttons: [] }
+    })));
+
+    const msgContent = {
+        viewOnceMessage: {
+            message: {
+                messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 },
+                interactiveMessage: {
+                    body: { text: '💥 *PINTEREST* 💥' },
+                    footer: { text: null },
+                    header: { hasMediaAttachment: false },
+                    carouselMessage: { cards: push }
+                }
+            }
+        }
+    };
 m.react(done)
 } catch {
 }}
