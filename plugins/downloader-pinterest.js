0c5a7c4ad82a1486b32d30c1b98e15c96f593842
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-21 10:21:50 -0500

    Actualizar downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,18 +1,17 @@
 import Scraper from "@SumiFX/Scraper"
-
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply(`👑 Ingresa un texto para realizar la búsqueda`)
-await m.react(rwait)
+if (!text) return m.reply('👑 Ingresa un texto para realizar la búsqueda.')
 try {
-let { dl_url } = await Starlights.pinterest(text)
+m.react(rwait)
+let { dl_url } = await Scraper.pinterest(text)
 await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*Resultado:* ${text}`, m, null, rcanal)
-await m.react(done)
+m.react(done)
 } catch {
-await m.react(error)
+m.react(error)
 }}
-handler.help = ['pinterest *<búsqueda>*']
+handler.help = ['pinterest <texto>']
 handler.tags = ['internet']
 handler.command = ['pinterest']
-handler.register = true 
 handler.limit = 1
+handler.register = true
 export default handler
