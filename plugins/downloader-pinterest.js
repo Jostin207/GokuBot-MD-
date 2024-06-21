dc9f664c6f1b5b454356dc4217f3c7234397aee8
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-21 10:27:23 -0500

    Actualizar downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -4,7 +4,7 @@ if (!text) return m.reply('👑 Ingresa un texto para realizar la búsqueda.')
 try {
 m.react(rwait)
 let { dl_url } = await Scraper.pinterest(text)
-await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*Resultado:* ${text}\n\n${global.textbot}`, m, null, rcanal)
+await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `💌 *Resultados De:* ${text}\n\n${global.textbot}`, m, null, rcanal)
 m.react(done)
 } catch {
 m.react(error)
