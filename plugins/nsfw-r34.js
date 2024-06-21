b1f8789b93910e0de15166fab922f2eab1c7ed94
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-21 13:56:33 -0500

    Actualizar nsfw-r34.js

--- plugins/nsfw-r34.js
+++ plugins/nsfw-r34.js
@@ -1,28 +1,28 @@
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args, usedPrefix }) => {
 if (!args[0]) {
-if (!db.data.chats[m.chat].modohorny && m.isGroup) return conn.reply(m.chat, `💌 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando ${usedPrefix}*`, m, rcanal)
-await conn.reply(m.chat, '🤍 Ingresa un tag para realizar la búsqueda.', m, rcanal);
+if (!db.data.chats[m.chat].modohorny && m.isGroup) return conn.reply(m.chat, `💌 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando ${usedPrefix}on modohorny*`, m, rcanal)
+await conn.reply(m.chat, '💌 Ingresa el nombre de la imágen que estas buscando', m, rcanal);
 return;
 }
-const tag = args[0];
-const url = `https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=${tag}`;
+const use = args[0];
+const url = `https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=${use}`;
 try {
 const response = await fetch(url);
 const data = await response.json();
 if (!data || data.length === 0) {
-await conn.reply(m.chat, `💌 No hubo resultados para *${tag}*`, m, rcanal);
+await conn.reply(m.chat, `💌 No hubo resultados para *${use}*`, m, rcanal);
 return;
 }
 const randomIndex = Math.floor(Math.random() * data.length);
 const randomImage = data[randomIndex];
-const imageUrl = randomImage.file_url;
-await conn.sendFile(m.chat, imageUrl, 'thumbnail.jpg', `*Resultados De:* ${tag}`, m, null, rcanal)
+const urlimg = randomImage.file_url;
+await conn.sendFile(m.chat, urlimg, 'thumbnail.jpg', `*Resultados De:* ${use}`, m, null, rcanal)
 } catch (error) {
 console.error(error);
 await m.reply('💥 Ocurrió un error inesperado.');
 }};
-handler.help = ['r34 <tag>'];
+handler.help = ['r34 <texto>'];
 handler.command = ['r34', 'rule34'];
 handler.tags = ['nsfw'];
 handler.register = true;
