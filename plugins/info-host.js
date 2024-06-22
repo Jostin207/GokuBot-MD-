05621e547fa227bf824a245f4e6e5a4b658d9076
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-21 22:57:51 -0500

    Actualizar info-host.js

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -1,4 +1,30 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+
+const sections = [{
+title: `Título de la sección`,
+rows: [
+{ header: 'Encabezado1', title: "Título1", description: 'Descripción1', id: usedPrefix + "menu" }, 
+{ header: 'Encabezado2', title: "Título2", description: 'Descripción2', id: "Id2" }, 
+{ header: 'Encabezado3', title: "Título3", description: 'Descripción3', id: "Id3" }, 
+{ header: 'Encabezado4', title: "Título4", description: 'Descripción4', id: "Id4" }, 
+]},]  
+const messages = [[ // CARRUSEL 1
+'Descripción de Carrusel 1', 
+'Footer de Carrusel 1',
+'https://telegra.ph/file/24b24c495b5384b218b2f.jpg',
+[['Botón1', usedPrefix + 'menu'], ['Botón2', 'Id2'] /* etc... */],
+[['Texto para copiar 1'], ['Texto para copiar 2'] /* etc... */],
+[['Enlace1', canal2], ['Enlace2', 'https://example.com/link2'] /* etc... */],
+[['Botón Lista 1', sections], ['Botón Lista 2', sections] /* etc... */]
+]]
+
+await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m)            
+
+}
+handler.command = /^(carousel)$/i
+export default handler
+
+/*let handler = async (m, { conn, command, usedPrefix }) => {
 let cafirexostxt = `
 _Optimice la implementación de *YoshikoBot* mediante la integración en un servicio de alojamiento de alto rendimiento._
 
@@ -89,4 +115,4 @@ sourceUrl: redesYoshi}}},
 }}
 
 handler.command = /^(cafirexos|infohost|hosting|infinitywa|infinity)$/i
-export default handler
+export default handler*/
