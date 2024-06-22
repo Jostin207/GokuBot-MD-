be1a0ffd543c411f922524e966fd84372d4870fc
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-21 21:33:57 -0500

    Actualizar owner-nuevonombre.js

--- plugins/owner-nuevonombre.js
+++ plugins/owner-nuevonombre.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) conn.reply(m.chat, '💌 *Que Nombre Deseas Ponerme?*', m, rcanal)
   try {
     await conn.updateProfileName(text)
-    throw conn.reply(m.chat, '✅️ *Nombre Cambiado Con Éxito*', m, rcanal)
+    throw conn.reply(m.chat, '✅️ *Nombre Cambiado Con Éxito*', m, null, rcanal)
   } catch (e) {
     console.log(e)
-    throw '🤍 Ocurrió Un Error¡!'
+    throw conn.reply(m.chat, '🤍 Ocurrió Un Error¡!', m, null, rcanal)
   }
 }
 handler.help = ['nuevonombre <teks>']
