daa69d741c15824500783276fa7563a57a44bb4f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-21 21:45:31 -0500

    Actualizar owner-nuevonombre.js

--- plugins/owner-nuevonombre.js
+++ plugins/owner-nuevonombre.js
@@ -14,20 +14,3 @@ handler.command = /^(nuevonombre|cambianombre)$/i
 
 handler.owner = true
 export default handler
-
-/*let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `💌 *Que Nombre Deseas Ponerme?*`
-  try {
-    await conn.updateProfileName(text)
-    m.reply('✅️ *Nombre Cambiado Con Éxito*')
-  } catch (e) {
-    console.log(e)
-    throw `🤍 Ocurrió Un Error¡!`
-  }
-}
-handler.help = ['nuevonombre <teks>']
-handler.tags = ['owner']
-handler.command = /^(nuevonombre|cambianombre)$/i
-
-handler.owner = true
-export default handler*/
