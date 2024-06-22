5d66e537cca76f4d14cc343452236afacb1f56d4
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-21 21:40:54 -0500

    Actualizar owner-nuevonombre.js

--- plugins/owner-nuevonombre.js
+++ plugins/owner-nuevonombre.js
@@ -1,4 +1,21 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) > return conn.reply(m.chat, `💌 *Que Nombre Deseas Ponerme?*`, m, rcanal)
+  try {
+    await conn.updateProfileName(text)
+    > return conn.reply(m.chat, '✅️ *Nombre Cambiado Con Éxito*', m, rcanal)
+  } catch (e) {
+    console.log(e)
+    throw `🤍 Ocurrió Un Error¡!`
+  }
+}
+handler.help = ['nuevonombre <teks>']
+handler.tags = ['owner']
+handler.command = /^(nuevonombre|cambianombre)$/i
+
+handler.owner = true
+export default handler
+
+/*let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `💌 *Que Nombre Deseas Ponerme?*`
   try {
     await conn.updateProfileName(text)
@@ -13,4 +30,4 @@ handler.tags = ['owner']
 handler.command = /^(nuevonombre|cambianombre)$/i
 
 handler.owner = true
-export default handler
+export default handler*/
