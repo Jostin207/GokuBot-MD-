1510eb6325d77e15c42458afc5458d755be9877d
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 11:14:16 -0500

    Actualizar rpg-acertijos.js

--- plugins/rpg-acertijos.js
+++ plugins/rpg-acertijos.js
@@ -3,22 +3,20 @@ const threshold = 0.72;
 const handler = (m) => m;
 handler.before = async function(m) {
   const id = m.chat;
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^🤍\*ACERTIJOS\*/i.test(m.quoted.text)) return true;
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('✧ Ese acertijo ya ha terminado!');
-  if (m.quoted.id === this.tekateki[id][0].id) {
+  if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
+  if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
-    if (m.text.toLowerCase() === json.response.toLowerCase().trim()) {
+    if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].limit += this.tekateki[id][2];
-      m.reply(`💥 Respuesta correcta, ganaste *+${this.tekateki[id][2]}* Centavos 🪙`);
+      m.reply(`🤍 *Respuesta correcta!*\n+${this.tekateki[id][2]} Centavos`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) {
-      m.reply('☁️ Casi lo logras!');
-    } else {
-      m.reply('🤍 Respuesta incorrecta!');
-    }
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
+    else m.reply('Respuesta incorrecta!');
   }
-  return true;
+  return !0;
 };
+handler.exp = 0;
 export default handler;
