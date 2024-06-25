3948989ac2c28d42acfaa79ff7ba3dbea29a3749
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 13:15:15 -0500

    Actualizar jadibot-token.js

--- plugins/jadibot-token.js
+++ plugins/jadibot-token.js
@@ -1,12 +1,12 @@
 import fs from "fs"
 async function handler(m, {usedPrefix}) {
     const user = m.sender.split("@")[0]
-    if (fs.existsSync("./YoshiJadiBot/" + user + "/creds.json")) {
-        let token = Buffer.from(fs.readFileSync("./YoshiJadiBot/" + user + "/creds.json"), "utf-8").toString("base64")
+    if (fs.existsSync("./ZeroJadiBot/" + user + "/creds.json")) {
+        let token = Buffer.from(fs.readFileSync("./ZeroJadiBot/" + user + "/creds.json"), "utf-8").toString("base64")
         await m.reply(`El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie.\n\n*Tu token es:*`)
         await m.reply(token)
     } else {
-        await m.reply(`*No Tienes Ningun Token Activo, Utiliza: ${usedPrefix}botclone Para Crear Un Token 📍.*`)
+        await m.reply(`*No Tienes Ningun Token Activo, Utiliza: ${usedPrefix}botclone Para Crear Un Token ☁️.*`)
     }
   }
   handler.command = handler.help = ['token', 'gettoken', 'serbottoken'];
