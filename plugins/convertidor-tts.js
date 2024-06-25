eaf7a6a1caba26a694f3e27d937afdf1347be391
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 13:07:32 -0500

    Actualizar convertidor-tts.js

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -16,7 +16,7 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
   } catch (e) {
     m.reply(e + '');
     text = args.join(' ');
-    if (!text) throw `*🍃 Te Faltó Un Texto*\n\nEjemplo:\n- !tts Hola Yoshiko`;
+    if (!text) throw `*🤍 Te Faltó Un Texto*\n\nEjemplo:\n- !tts Hola Re:zero`;
     res = await tts(text, defaultLang);
   } finally {
     if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true);
