49ccc892349259ee7c6cecc7d0570b0621ae23c3
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-26 15:09:45 -0500

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw '⚠ *_️Ingrese el error ue desea reportar._*'
     if (text.length < 10) throw '⚠️ *_Especifique bien el error, mínimo 10 caracteres._*'
     if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar el error._*'
-    const teks = `╭───────────────────\n│⊷〘 *R E P O R T E* 🤍 〙⊷\n├───────────────────\n│🍄 *Cliente:*\n│✏️ Wa.me/${m.sender.sPlit`@`[0]}\n│\n│📮 *Mensaje:*\n│📩 ${text}\n╰───────────────────`
+    const teks = `╭───────────────────\n│⊷〘 *R E P O R T E* 🤍 〙⊷\n├───────────────────\n│🍄 *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│📮 *Mensaje:*\n│📩 ${text}\n╰───────────────────`
     await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
     m.reply('⚠️ *_El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo._*')
 }
