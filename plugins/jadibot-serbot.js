4b9778612e7fa59d9e82f7ba835c707285f28646
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-26 18:10:18 -0500

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -11,7 +11,7 @@ console.log()
 global.conns = []
 }
 let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner, isPrems, isROwner }) => {
-if (!global.db.data.settings[conn.user.jid].jadibotmd && !isRowner ) {
+if (!global.db.data.settings[conn.user.jid].jadibotmd && !isROwner ) {
 conn.reply(m.chat, '🤍 Este Comando está deshabilitado por mi creador.', m, rcanal)
 return
 }
