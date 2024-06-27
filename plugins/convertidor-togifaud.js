cb1501e3793596df0d9c63ae78fb293b952bd95c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-27 10:39:07 -0500

    Actualizar convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -3,7 +3,12 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const q = m.quoted || m;
   const mime = (q.msg || q).mimetype || '';
   if (!/(mp4)/.test(mime)) throw `*☄️ El Tipo De Archivo ${mime} No Es Correcto, Responda A Un Video Que Desee Convertir En Gif Con Audio*`;
-  m.reply(global.wait);
+  conn.reply(m.chat, wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: redes }}})
   const media = await q.download();
   conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*☁️ La Musica Se Reproduce Cuando Abras El Archivo*'}, {quoted: m});
 };
