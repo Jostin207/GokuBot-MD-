665fbfefb3d5eb13c9e9acac97a6c42dc9dc8a04
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-27 10:17:57 -0500

    Actualizar downloader-imagen.js

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -1,14 +1,14 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Re:zero*`;
+  conn.reply(m.chat, `⏱️ *ESPERE UN MOMENTO!....*`, m, {
+  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+  title: packname,
+  body: wm,
+  previewType: 0, thumbnail: icons,
+  sourceUrl: redes }}})
   const res = await googleImage(text);
   const image = await res.getRandom();
-conn.reply(m.chat, `💋 *Buscando Imagen....*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: redes }}})
   const link = image;
   conn.sendFile(m.chat, link, 'error.jpg', `*🔎 Resultado De: ${text}*`, m);
 };
