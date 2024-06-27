9e2cc53b932ab84c33c098b992c586820f943991
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-26 20:26:47 -0500

    Actualizar bot-autobio.js

--- plugins/bot-autobio.js
+++ plugins/bot-autobio.js
@@ -2,8 +2,7 @@ const handler = m => m
 handler.all = async function (m) {
 //if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${global.wm} EL AUTOBIO ESTÁ DESACTIVADO, ACTIVALO USANDO #on autobio`
 if (db.data.settings[this.user.jid].autobio) {
-return
-}
+
 let _uptime = process.uptime() * 1000
 let _muptime
 if (process.send) { process.send('uptime')
