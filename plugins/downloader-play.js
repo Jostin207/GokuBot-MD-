649142cb9dcd596d38185316a25d7aeb3255ca48
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-27 10:48:57 -0500

    Actualizar downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -11,6 +11,13 @@ m.react(rwait)
 
 try {
 
+conn.reply(m.chat, wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: redes }}})
+
 const yt_play = await search(args.join(' '))
 let additionalText = ''
 if (command === 'play') {
