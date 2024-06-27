a6332f7216684f2e8deb3156a5331a519ba6412a
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-26 21:20:13 -0500

    Actualizar owner-nuevabiobot.js

--- plugins/owner-nuevabiobot.js
+++ plugins/owner-nuevabiobot.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-   if (!text) conn.reply(m.chat, '🤍 *¡Te faltó el texto mi propietario!*', m, rcanal)
+   if (!text) return conn.reply(m.chat, '🤍 *¡Te faltó el texto mi propietario!*', m, rcanal)
      try {
                 await conn.updateProfileStatus(text).catch(_ => _)
                 conn.reply(m.chat, `✅️ Info Cambiada Con Exito!`, m, rcanal)
