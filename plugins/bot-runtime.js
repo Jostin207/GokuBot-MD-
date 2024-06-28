c910afa6424a8683c409924dfa1743b2a7821db5
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-27 19:34:35 -0500

    Actualizar bot-runtime.js

--- plugins/bot-runtime.js
+++ plugins/bot-runtime.js
@@ -22,13 +22,9 @@ const time = dd.toLocaleString('en-US', {
 
 function rTime(seconds) {
   seconds = Number(seconds);
-  var d = Math.floor(seconds / (3600 * 24));
-  var h = Math.floor((seconds % (3600 * 24)) / 3600);
-  var m = Math.floor((seconds % 3600) / 60);
-  var s = Math.floor(seconds % 60);
-  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " : ") : "";
-  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " : ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " : " : " : ") : "";
-  var sDisplay = s > 0 ? s + (s == 1 ? " " : " ") : "";
-  return dDisplay + hDisplay + mDisplay + sDisplay;
-};
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
