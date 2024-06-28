1dfad5d79d5670a2b1f3fc0751ee0667b10ba09f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-28 13:08:55 -0500

    Actualizar convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,4 +1,19 @@
-const handler = async (m, {conn, usedPrefix, command}) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+if (!m.quoted) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m, rcanal)
+const q = m.quoted || m
+let mime = (q.msg || q).mimetype || ''
+if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m, rcanal)
+await m.react(rwait)
+let media = await q.download()
+conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: listo }, { quoted: fkontak })
+await m.react(done)
+}
+handler.help = ['togifaud']
+handler.tags = ['tools']
+handler.command = ['togifaud']
+export default handler
+
+/* const handler = async (m, {conn, usedPrefix, command}) => {
   if (!m.quoted) throw `*☁️ Responda A Un Video Que Desee Convertir En Gif Con Audio*`;
   const q = m.quoted || m;
   const mime = (q.msg || q).mimetype || '';
@@ -13,4 +28,4 @@ sourceUrl: channel }}})
   conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*☁️ La Musica Se Reproduce Cuando Abras El Archivo*'}, {quoted: m});
 };
 handler.command = ['togifaud'];
-export default handler;
+export default handler; */
