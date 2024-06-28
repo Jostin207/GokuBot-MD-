71761d1bdce77dffbe91ebbab768ae4f6e77395f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-28 09:11:47 -0500

    Update and rename downloader.audio.js to downloader-audio.js

--- plugins/downloader-audio.js
+++ plugins/downloader-audio.js
@@ -3,19 +3,19 @@ import yts from 'yt-search'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 let limit = 100
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!m.quoted) return m.reply('Etiqueta el mensaje que contenga el resultado de YouTube Play.')
-if (!m.quoted.text.includes(`${packname}`)) return m.reply('Etiqueta el mensaje que contenga el resultado de YouTube Play.')
+if (!m.quoted) return conn.reply(m.chat, 'Etiqueta el mensaje que contenga el resultado de YouTube Play.', m, rcanal)
+if (!m.quoted.text.includes(`${packname}`)) return conn.reply(m.chat, 'Etiqueta el mensaje que contenga el resultado de YouTube Play.', m, rcanal)
 if (!m.quoted.isBaileys) return m.reply('Etiqueta un mensaje mio que contenga el resultado de YouTube Play.')
 let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
 if (!urls) return m.reply('×')
-if (urls.length < text) return m.reply('Resultado no Encontrado.')
+if (urls.length < text) return conn.reply(m.chat, 'Resultado no Encontrado.', rcanal)
 let q = '128kbps'
 try {
 await m.react(rwait)
 const yt = await fg.yta(urls[0], q)
 let { title, dl_url, size } = yt 
 
-if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`) 
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
 conn.reply(m.chat, `⏱️ *Descargando El Audio*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
@@ -30,7 +30,7 @@ try {
 let yt = await fg.ytmp3(urls[0], q)
 let { title, size, dl_url } = yt
 
-if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`) 
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat  `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
 
 await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: fkontak })
 await m.react(done)
