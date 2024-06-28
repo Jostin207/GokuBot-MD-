5f6d53d459d9d6c3eaa3c3810c87ddc48d3287ff
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-27 19:32:26 -0500

    Actualizar bot-runtime.js

--- plugins/bot-runtime.js
+++ plugins/bot-runtime.js
@@ -26,9 +26,9 @@ function rTime(seconds) {
   var h = Math.floor((seconds % (3600 * 24)) / 3600);
   var m = Math.floor((seconds % 3600) / 60);
   var s = Math.floor(seconds % 60);
-  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : ": ") : "";
-  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : ": ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : ": ") : "";
+  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " : ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " : ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " : ") : "";
   var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " ") : "";
   return dDisplay + hDisplay + mDisplay + sDisplay;
 };
