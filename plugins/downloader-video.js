3940b2a896d734cd1a184efd64235a3045af5ea0
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-28 09:27:34 -0500

    Actualizar downloader-video.js

--- plugins/downloader-video.js
+++ plugins/downloader-video.js
@@ -3,19 +3,19 @@ import yts from 'yt-search'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 let limit = 350
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, 'Etiqueta el mensaje que contenga el resultado de YouTube Play.', m, rcanal)
-if (!m.quoted.text.includes(`${packname}`)) return conn.reply(m.chat, 'Etiqueta el mensaje que contenga el resultado de YouTube Play.', m, rcanal)
-if (!m.quoted.isBaileys) return conn.reply(m.chat, 'Etiqueta un mensaje mio que contenga el resultado de YouTube Play.', m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, '💜 *Etiquete el mensaje que contenga el resultado del Play*', m, rcanal)
+if (!m.quoted.text.includes(`${packname}`)) return conn.reply(m.chat, '💜 *Etiquete el mensaje que contenga el resultado del Play*', m, rcanal)
+if (!m.quoted.isBaileys) return conn.reply(m.chat, '🧡 Etiqueta el mensaje mío del resultado Play', m, rcanal)
 let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
 if (!urls) return m.reply('×')
-if (urls.length < text) return conn.reply(m.chat, 'Resultado no Encontrado.', m, rcanal)
+if (urls.length < text) return conn.reply(m.chat, '😺 *No se encontraron resultados*', m, rcanal)
 let q = urls[1] || '480p'
 try {
 await m.react(rwait)
 const yt = await fg.ytv(urls[0], q)
 let { title, dl_url, size } = yt 
 
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `✨️ El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
 
 conn.reply(m.chat, `⏱️ *Descargando El Video*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
@@ -30,12 +30,12 @@ try {
 let yt = await fg.ytmp4(urls[0], q)
 let { title, size, dl_url } = yt
 
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `✨️ El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
 
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: fkontak })
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `✨️ *Tu video de Youtube*\n${packname}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: fkontak })
 await m.react(done)
 } catch {
-await m.reply(`✘ Ocurrío un error`)
+await m.reply(`✘ *Ocurrío un error*`)
 }}}
 handler.help = ['Video']
 handler.tags = ['downloader']
