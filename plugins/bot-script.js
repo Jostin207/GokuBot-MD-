9e7d3b24ca3a039c573ce57489f120ac9796a3c8
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-28 12:08:15 -0500

    Actualizar bot-script.js

--- plugins/bot-script.js
+++ plugins/bot-script.js
@@ -14,9 +14,8 @@ let txt = `*乂  S C R I P T  -  M A I N*\n\n`
     txt += `        ✩   *Forks* : ${json.forks_count}\n`
     txt += `        ✩   *Stars* : ${json.stargazers_count}\n\n`
     txt += `> 🚩 *${packname}*`
-let img = await (await fetch(imagen1)).buffer()
 
-await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": packname, "body": wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "mediaUrl": redes, "sourceUrl": redes}}}, {quoted: fkontak});
+await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": packname, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": redes, "sourceUrl": redes}}}, {quoted: fkontak});
 //await conn.reply(m.chat, botname, textbot, txt, img, img, redes, m)
 } catch {
 await m.react('✖️')
