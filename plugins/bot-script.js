4a78e74bae25ca6f6f884231396911c7d05bdf7c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-28 12:04:09 -0500

    Actualizar bot-script.js

--- plugins/bot-script.js
+++ plugins/bot-script.js
@@ -1,18 +1,27 @@
-import moment from 'moment-timezone';
-import fetch from 'node-fetch';
-const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/OfcDiego/EmiliaRezero-MD');
-   const json = await res.json();
-   let txt = `           *乂  B O T S C R I P T  乂*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || 'EmiliaRezero-MD'}\n`;
-      txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
-      txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
-      txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
-      txt += `◦  *Url:* ${json?.html_url || 'https://github.com/OfcDiego/EmiliaRezero-MD'}\n\n`;
-      txt += `         ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.botname, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen2, "mediaUrl": `https://github.com/OfcDiego/EmiliaRezero-MD`, "sourceUrl": `https://github.com/OfcDiego/EmiliaRezero-MD`}}}, {quoted: fkontak});
-};
-handler.help = ['scbot'];
-handler.tags = ['info'];
-handler.command = /^(sc|scbot|scrip|script)$/i;
-export default handler;
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args }) => {
+let res = await fetch('https://api.github.com/repos/OfcDiego/EmiliaRezero-MD')
+let json = await res.json()
+try {
+let txt = `*乂  S C R I P T  -  M A I N*\n\n`
+    txt += `        ✩   *Nombre* : ${json.name}\n`
+    txt += `        ✩   *Visitas* : ${json.watchers_count}\n`
+    txt += `        ✩   *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
+    txt += `        ✩   *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+    txt += `        ✩   *Url* : ${json.html_url}\n`
+    txt += `        ✩   *Forks* : ${json.forks_count}\n`
+    txt += `        ✩   *Stars* : ${json.stargazers_count}\n\n`
+    txt += `> 🚩 *${packname}*`
+let img = await (await fetch(imagen1)).buffer()
+
+await conn.reply(m.chat, botname, textbot, txt, img, img, redes, m, rcanal)
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['script']
+handler.tags = ['main']
+handler.command = ['script', 'sc']
+handler.register = true 
+export default handler
