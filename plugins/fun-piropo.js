ce3cf9156d2e59876ca697fb5df595af599bfb0c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 10:14:50 -0500

    Actualizar fun-piropo.js

--- plugins/fun-piropo.js
+++ plugins/fun-piropo.js
@@ -4,7 +4,7 @@ import db from '../lib/database.js'
 
 let handler = async (m, { conn, text}) => {
 
-conn.reply(m.chat, '🤍 *Buscando Un Piropo!*', m, {
+conn.reply(m.chat, '🤍 *¡Buscando un piropo!*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
