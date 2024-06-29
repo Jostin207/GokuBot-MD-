65aeb9f7ad68b842a40122d71714d7cd14d9ac0d
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 14:02:30 -0500

    Actualizar jadibot-serbot-info.js

--- plugins/jadibot-serbot-info.js
+++ plugins/jadibot-serbot-info.js
@@ -1,25 +1,23 @@
 import ws from 'ws'
-import fetch from 'node-fetch'
+async function handler(m, { conn: stars, usedPrefix }) {
+  let uniqueUsers = new Map()
+  global.conns.forEach((conn) => {
+    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+      uniqueUsers.set(conn.user.jid, conn)
+    }
+  })
+ let users = [...uniqueUsers.values()]
+ let message = Array.from(uniqueUsers.values()).map((user, index) => `╭─⬣「 *Emilia Re:zero - MD* 🤍 」⬣\n│⁖ฺ۟̇࣪·֗٬̤⃟🌸 *${index + 1}.-* @${user.jid.replace(/[^0-9]/g, '')}\n│❀ *Link:* https://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n│❀ *Nombre:* ${user.name || '𝚂𝚄𝙱-𝙱𝙾𝚃'}\n╰─⬣\n`).join('\n')
+  let replyMessage = message.length === 0 ? '' : message
+  global.totalUsers = users.length
+  let responseMessage = `╭───────────────────\n│⊷〘 *${global.wm}* 〙⊷\n│ 💫 *Total De Sub-Bots: ${totalUsers || '0'}\n╰───────────────────\n\n${replyMessage.trim()}`.trim()
+  await stars.sendMessage(m.chat, { image: { url: "https://telegra.ph/file/b867188ad3e6ea1f593ad.png" }, caption: responseMessage, mentions: stars.parseMention(responseMessage), quoted: m})
 
-async function handler(m, { conn: _envio, usedPrefix }) {
-const uniqueUsers = new Map()
-
-global.conns.forEach((conn) => {
-if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
-
-const message = Array.from(uniqueUsers.values()).map((user, index) => `╭─⬣「 *Emilia Re:zero - MD* 🤍 」⬣\n│⁖ฺ۟̇࣪·֗٬̤⃟🌸 *${index + 1}.-* @${user.jid.replace(/[^0-9]/g, '')}\n│❀ *Link:* https://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n│❀ *Nombre:* ${user.name || '𝚂𝚄𝙱-𝙱𝙾𝚃'}\n╰─⬣\n`).join('\n')
-
-const replyMessage = message.length === 0 ? "" : message
-const totalUsers = uniqueUsers.size;
-const responseMessage = `${` 💙 *S E R B O T - J A D I B O T* 💙\n\n${replyMessage.trim()}`.trim()}`
-
-let img = await (await fetch(`https://telegra.ph/file/5d07a1f6d08a147c6f0f9.png`)).buffer()
-
-await _envio.sendFile(m.chat, img, 'thumbnail.jpg', responseMessage, m, false, { mentions: _envio.parseMention(responseMessage) })
+  //await stars.reply(m.chat, responseMessage, m, { mentions: await stars.parseMention(responseMessage)} )
 }
 
-handler.command = ['listjadibot', 'bots']
+handler.command = ['bots']
 handler.help = ['bots']
 handler.tags = ['jadibot']
+handler.registrado = true
 export default handler
