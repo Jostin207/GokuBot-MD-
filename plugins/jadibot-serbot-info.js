9b65e3def07709be05b27cf9b53307d7bec295d9
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 14:03:37 -0500

    Actualizar jadibot-serbot-info.js

--- plugins/jadibot-serbot-info.js
+++ plugins/jadibot-serbot-info.js
@@ -10,7 +10,7 @@ async function handler(m, { conn: stars, usedPrefix }) {
  let message = Array.from(uniqueUsers.values()).map((user, index) => `╭─⬣「 *Emilia Re:zero - MD* 🤍 」⬣\n│⁖ฺ۟̇࣪·֗٬̤⃟🌸 *${index + 1}.-* @${user.jid.replace(/[^0-9]/g, '')}\n│❀ *Link:* https://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n│❀ *Nombre:* ${user.name || '𝚂𝚄𝙱-𝙱𝙾𝚃'}\n╰─⬣\n`).join('\n')
   let replyMessage = message.length === 0 ? '' : message
   global.totalUsers = users.length
-  let responseMessage = `╭───────────────────\n│⊷〘 *${global.wm}* 〙⊷\n│ 💫 *Total De Sub-Bots: ${totalUsers || '0'}\n╰───────────────────\n\n${replyMessage.trim()}`.trim()
+  let responseMessage = `╭───────────────────\n│⊷〘 *${global.wm}* 〙⊷\n│ 💫 *Total De Sub-Bots:* ${totalUsers || '0'}\n╰───────────────────\n\n${replyMessage.trim()}`.trim()
   await stars.sendMessage(m.chat, { image: { url: "https://telegra.ph/file/b867188ad3e6ea1f593ad.png" }, caption: responseMessage, mentions: stars.parseMention(responseMessage), quoted: m})
 
   //await stars.reply(m.chat, responseMessage, m, { mentions: await stars.parseMention(responseMessage)} )
