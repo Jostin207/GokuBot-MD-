f5f1af52874911f9413227e34141e272e4919875
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 20:10:40 -0500

    ⁖💚꙰༻ 𝗘𝗺𝗶𝗹𝗶𝗮 𝗥𝗲:𝘇𝗲𝗿𝗼 - 𝗨𝗽𝗱𝗮𝘁𝗲 ⁖💚꙰༻

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -10,7 +10,7 @@ watch
 } from 'fs'
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
 
-conn.reply(m.chat, `🧰 Realizado, ya se ha eliminado los archivos de la carpeta tmp`, m, rcanal, )
+conn.reply(m.chat, `⁖🧡꙰  Realizado, ya se ha eliminado los archivos de la carpeta tmp`, m, rcanal, )
 
 const tmp = [tmpdir(), join(__dirname, '../tmp')]
 const filename = []
