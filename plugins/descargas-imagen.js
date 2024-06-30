53207e5ef385abbb0d37ed4d89e62692222241be
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 19:44:29 -0500

    Actualizar descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,7 +1,7 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Re:zero*`;
-  conn.reply(m.chat, wait, m, {
+  conn.reply(m.chat, '⁖💚꙰  *Descargando su imagen...*', m, {
   contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
   title: packname,
   body: wm,
