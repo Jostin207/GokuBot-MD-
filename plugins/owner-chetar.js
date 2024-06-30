25a8a74f825f3f2fb415b0e4646927d5ceda2133
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 20:10:15 -0500

    ⁖💚꙰༻ 𝗘𝗺𝗶𝗹𝗶𝗮 𝗥𝗲:𝘇𝗲𝗿𝗼 - 𝗨𝗽𝗱𝗮𝘁𝗲 ⁖💚꙰༻

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `*👑 @${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: m});
+        conn.sendMessage(m.chat, {text: `*⁖💚꙰  @${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: m});
       global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].limit = Infinity;
   global.db.data.users[m.sender].level = Infinity;
