d2d479738f7e732dab54e385dd8624022d86967b
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 19:29:00 -0500

    ⁖💚꙰༻ 𝗘𝗺𝗶𝗹𝗶𝗮 𝗥𝗲:𝘇𝗲𝗿𝗼 - 𝗨𝗽𝗱𝗮𝘁𝗲 ⁖💚꙰༻

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,7 +2,7 @@ import yts from 'yt-search'
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
 
-if (!text) return conn.reply(m.chat, `⁖🧡꙰ *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Emilia Re:zero`, m, rcanal, )
+if (!text) return conn.reply(m.chat, `⁖🧡꙰  *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Emilia Re:zero`, m, rcanal, )
 
 conn.reply(m.chat, wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
