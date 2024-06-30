9e2ddbf908816016cb6e5f3d4d9540023961a60e
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 19:27:03 -0500

    Actualizar youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch"
 import yts from "yt-search"
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `⁖💚꙰༻ *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`,  m, rcanal, )
+if (!text) return conn.reply(m.chat, `💜 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`,  m, rcanal, )
 conn.reply(m.chat, global.wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
