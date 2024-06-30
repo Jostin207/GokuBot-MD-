8c5a94ad716118ad341dd363a0e375f9edfc01e8
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-29 20:07:53 -0500

    ⁖💚꙰༻ 𝗘𝗺𝗶𝗹𝗶𝗮 𝗥𝗲:𝘇𝗲𝗿𝗼 - 𝗨𝗽𝗱𝗮𝘁𝗲 ⁖💚꙰༻

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -1,9 +1,9 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('💌 *¡YA ERES ADM JEFE!* 💌');
+  if (isAdmin) return m.reply('⁖💚꙰  *¡YA ERES ADM JEFE!*  ⁖💚꙰');
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
     m.react('✅')
-   m.reply('👑 *¡YA TE DI ADM MI JEFE!* 👑');
+   m.reply('⁖🧡꙰  *¡YA TE DI ADM MI JEFE!*  ⁖🧡꙰');
     let nn = conn.getName(m.sender);
      conn.reply('573012482597@s.whatsapp.net', `💥 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, fake, );
   } catch {
