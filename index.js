ddaa1183820b56b3c6de6fec97bb41cfdaeeaa88
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-30 22:08:21 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('✰ Iniciando La Bot ✰')
+console.log('✰ Iniciando LuffyBot ✰')
 import { join, dirname } from 'path'
 import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Emilia\nRezero-MD', {
+say('Luffy\nBot-MD', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -75,4 +75,4 @@ p.emit('message', line.trim())
 })
 }
 
-start('rezero.js')
+start('luffy.js')
