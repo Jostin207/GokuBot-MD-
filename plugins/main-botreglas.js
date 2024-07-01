7f44305d9dca1f9d9206fd3a2c69a9da40f5baa5
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-30 22:32:48 -0500

    Actualizar main-botreglas.js

--- plugins/main-botreglas.js
+++ plugins/main-botreglas.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let zero = `⁖🩵꙰  *Respeta las reglas de Emilia Re:zero*\n
+let luffy = `⁖🩵꙰  *Respeta las reglas de LuffyBot*\n
 ✰ No llamar A La Bot
 ✰ No hacer spam
 ✰ Contacta al creador si es necesario
@@ -10,10 +10,10 @@ let zero = `⁖🩵꙰  *Respeta las reglas de Emilia Re:zero*\n
 
 ⁖❤️꙰  𝗡𝗼𝘁𝗮: *Si no cumples con las reglas de la bot, seras bloqueado.*
 
-⁖💚꙰  𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Emilia.*
+⁖💚꙰  𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Luffy.*
 
 ${global.md}`.trim()
-await conn.reply(m.chat, zero, m, fake, )
+await conn.reply(m.chat, luffy, m, fake, )
 
 }
 handler.help = ['botreglas']
