2a1df5b8d0d3a51964b996c766931ca2c8681b57
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-01 08:56:40 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -10,7 +10,7 @@ return conn.reply(m.chat, '💌 *Utiliza este comando directamente en el número
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
-let sessionPath = './ZeroSession/'
+let sessionPath = './LuffySession/'
 
 try {
 
