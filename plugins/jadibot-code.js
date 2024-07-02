98a75d683609e067d913f901739a851d3cb72809
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-01 23:00:24 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/jadibot-code.js
+++ plugins/jadibot-code.js
@@ -67,7 +67,7 @@ process.exit(0);
 setTimeout(async () => {
 let codeBot = await conn.requestPairingCode(cleanedNumber);
 codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot;
-let txt = `✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉`;
+let txt = '🚩 S E R B O T - S U B B O T 🚩\n\n*Usa este Código para convertirte en un Sub Bot*\n\n🐢 Pasos\n\n`1` : Haga click en los 3 puntos\n\n`2` : Toque dispositivos vinculados\n\n`3` : Selecciona Vincular con el número de teléfono\n\n`4` : Escriba el Codigo\n\n> *Nota:* Este Código solo funciona en el número que lo solicito.';
 await parent.reply(m.chat, txt, m, rcanal);
 await parent.reply(m.chat, codeBot, m, rcanal);
 rl.close();
