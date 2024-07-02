5655844d1a656ade1bc5c1f6c7a9a7a5ad82b80c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-02 09:22:24 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -17,7 +17,7 @@ let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
 
 let img = imagen1
 
-await conn.sendLuffy(m.chat, packname, wm, txt, img, img, redes, m, rcanal)
+await conn.sendLuffy(m.chat, packname, wm, txt, img, img, redes, m)
 } catch {
 await m.react(error)
 }}
