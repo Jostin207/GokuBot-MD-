d39b9d316ca56db4503c232f3a924cc0fb38a5a8
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-02 08:17:13 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/info-speed.js
+++ plugins/info-speed.js
@@ -37,7 +37,7 @@ let texto = `☘️ *${global.botname}*
 m.react('✈️')
 
 // conn.reply(m.chat, texto, m, fake, )
-await conn.sendFile(m.chat, icons, 'luffy.jpg', texto, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: packname, body: wm, previewType: 0, thumbnail: icons, sourceUrl: redes }}})
+await conn.sendFile(m.chat, icons, 'luffy.jpg', texto, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: packname, body: wm, previewType: 0, thumbnail: null, sourceUrl: redes }}})
 
 }
 handler.help = ['speed']
