cab966cab9b61b3930ab850816b73371cb4686ba
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-03 12:03:21 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -1,6 +1,7 @@
-// import Scraper from '@SumiFX/Scraper'
+import Starlights from '@StarlightsTeam/Scraper'
+let listo = '🚩 Tu video de instagram'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('💥 Ingresa un enlace de Instagram.')
+if (!args[0]) return conn.reply(m.chat, '🚩 Ingresa un enlace de Instagram.', m, rcanal)
 try {
 conn.reply(m.chat, '⁖❤️꙰  *Descargando su video de Instagram*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
@@ -8,17 +9,15 @@ title: packname,
 body: wm,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
-let { dl_url } = await Scraper.igdl(args[0])
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `✅️ *Su Video De Instagram*\n${botname}` }, { quoted: fkontak})
+let { dl_url } = await Starlights.igdl(args[0])
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: listo }, { quoted: fkontal})
 } catch (e) {
   console.log(e)
-  m.reply('⁖🩵꙰  Ocurrió un error inesperado.')
+  m.reply('🚩 Ocurrió un error inesperado.')
 }}
-
 handler.help = ['ig <enlace>']
 handler.tags = ['descargas']
 handler.command = ['ig', 'instagram']
 handler.register = true
 handler.estrellas = 1
-
 export default handler
