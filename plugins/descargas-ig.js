db84a430c4c4e76060f093e7a0091d33418de810
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-03 11:54:25 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -1,8 +1,6 @@
 import fetch from 'node-fetch'
 import axios from 'axios'
-import instagramGetUrl from 'instagram-url-direct'
 import { instagram } from '@xct007/frieren-scraper'
-import { instagramdl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) return m.reply('💥 Ingresa un enlace de Instagram.')
 try {
