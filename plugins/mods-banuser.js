9b275ea2b38b554a10da4f1c188ba2f406c1c2c4
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-07 07:33:44 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/mods-banuser.js
+++ plugins/mods-banuser.js
@@ -13,7 +13,7 @@ number = text
 }
 user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
 bot = conn.user.jid.split`@`[0] 
-bant = `☄️ *Etiquete a una persona*\n\nEjemplo, !${command} @${global.suittag}`
+bant = `🚩 *Etiquete a una persona*\n\nEjemplo, !${command} @${global.suittag}`
 const nn = conn.getName(m.sender);
 if (!text && !m.quoted) return conn.reply(m.chat, bant, m, { mentions: [user] })               
 try {
@@ -26,20 +26,20 @@ user = number + '@s.whatsapp.net'
 }} catch (e) {
 } finally {
 number = user.split('@')[0]
-if(user === conn.user.jid) return conn.reply(m.chat, `☄️ @${bot} *No puede ser baneado con este comando*`, m, { mentions: [user] })   
+if(user === conn.user.jid) return conn.reply(m.chat, `🚩 @${bot} *No puede ser baneado con este comando*`, m, { mentions: [user] })   
 for (let i = 0; i < global.owner.length; i++) {
 ownerNumber = global.owner[i][0];
 if (user.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
 aa = ownerNumber + '@s.whatsapp.net'
-await conn.reply(m.chat, `☄️ *No puedo banear al propietario @${ownerNumber} de ${wm}*`, m, { mentions: [aa] })
+await conn.reply(m.chat, `🚩 *No puedo banear al propietario @${ownerNumber} de ${wm}*`, m, { mentions: [aa] })
 return
 }}
 users = global.db.data.users
-if (users[user].banned === true) conn.reply(m.chat, `☄️ *No es necesario volver a banear a @${number}*`, m, { mentions: [user] }) 
+if (users[user].banned === true) conn.reply(m.chat, `🚩 *No es necesario volver a banear a @${number}*`, m, { mentions: [user] }) 
 users[user].banned = true
 usr = m.sender.split('@')[0]     
 await conn.reply(m.chat, '✅ *Usuario baneado con éxito*', m, { mentions: [user] })   
-await conn.reply('573012482597@s.whatsapp.net', `💭 El usuario *${nametag}* ha sido Baneado por *${nn}*.`, m, rcanal, );
+await conn.reply('573012482597@s.whatsapp.net', `🚩 El usuario *${nametag}* ha sido Baneado por *${nn}*.`, m, rcanal, );
 }} catch (e) {
 await conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, rcanal, )
 console.log(e) 
