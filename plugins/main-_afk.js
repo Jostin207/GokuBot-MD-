b345d4afc9bc06a2463fe2ace99f054224bc9f21
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-07 07:28:39 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/main-_afk.js
+++ plugins/main-_afk.js
@@ -1,26 +1,22 @@
 export function before(m) {
-  const user = global.db.data.users[m.sender];
-  if (user.afk > -1) {
-    m.reply(`
-  _📍 Dejastes De Estar Inactivo_\n${user.afkReason ? 'Motivo De La Inactividad: ' + user.afkReason : ''}
-  
-  *Tiempo Inactivo: ${(new Date - user.afk).toTimeString()}*
-  `.trim());
-    user.afk = -1;
-    user.afkReason = '';
-  }
-  const jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])];
-  for (const jid of jids) {
-    const user = global.db.data.users[jid];
-    if (!user) {
-      continue;
-    }
-    const afkTime = user.afk;
-    if (!afkTime || afkTime < 0) {
-      continue;
-    }
-    const reason = user.afkReason || '';
-    m.reply(`*📍 El Usuario Esta Inactivo No Lo Etiquetes 🥀*`.trim());
-  }
-  return true;
+const user = global.db.data.users[m.sender];
+if (user.afk > -1) {
+conn.reply(m.chat, `🚩 Dejastes De Estar Inactivo\n${user.afkReason ? 'Motivo De La Inactividad: ' + user.afkReason : ''}\n\n*Tiempo Inactivo: ${(new Date - user.afk).toTimeString()}*`, m, rcanal)
+user.afk = -1;
+user.afkReason = '';
+}
+const jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])];
+for (const jid of jids) {
+const user = global.db.data.users[jid];
+if (!user) {
+continue;
+}
+const afkTime = user.afk;
+if (!afkTime || afkTime < 0) {
+continue;
+}
+const reason = user.afkReason || '';
+conn.reply(m.chat, `🚩 *El Usuario Esta Inactivo No Lo Etiquetes*`, m, rcanal)
+}
+return true;
 }
