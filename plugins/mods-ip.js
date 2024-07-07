d1c54ff9bf3d930f84d7449878d3d421337002ef
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-07 07:36:47 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/mods-ip.js
+++ plugins/mods-ip.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text }) => {
 //await m.reply('🧑🏻‍💻 Buscando...')
 let bot = '🧑🏻‍💻 Buscando....'
 conn.reply(m.chat, bot, m, rcanal, )
-  if (!text) return conn.reply(m.chat, '⁖💚꙰  *Te Faltó La <Ip>*', m, rcanal, )
+  if (!text) return conn.reply(m.chat, '🚩 *Te Faltó La <Ip>*', m, rcanal, )
 
   axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`).then ((res) => {
     const data = res.data
@@ -17,7 +17,7 @@ conn.reply(m.chat, bot, m, rcanal, )
         throw new Error(data.message || "Falló")
       }
     let ipsearch = `
-⁖💚꙰  *I N F O - I P* ⁖💚꙰
+☁️ *I N F O - I P* ☁️
 
 IP : ${data.query}
 País : ${data.country}
