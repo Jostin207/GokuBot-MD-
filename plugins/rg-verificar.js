cfe29e43ce96a4fb581bba018883430a6e972ba4
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-07 18:31:50 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -40,7 +40,7 @@ contextInfo: {
 externalAdReply: {
 title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',
 body: wm, 
-thumbnaiUrl: icons, 
+thumbnaiUrl: 'https://qu.ax/repp.jpg', 
 sourceUrl: canales,
 mediaType: 1,
 showAdAttribution: true,
