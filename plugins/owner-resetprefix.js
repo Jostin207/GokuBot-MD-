fe5cba235ca0658d685cfa1c997be2fbdf5e93eb
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-07 07:44:49 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/owner-resetprefix.js
+++ plugins/owner-resetprefix.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn}) => {
   global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
   //await m.reply(`✅️ *Prefijo Restablecido Con Éxito!*`);
-  conn.fakeReply(m.chat, '✅️ *Prefijo Restablecido Con Éxito!*', '0@s.whatsapp.net', '💌 PREFIJO RESTABLECIDO 💌')
+  conn.fakeReply(m.chat, '✅️ *Prefijo Restablecido Con Éxito!*', '0@s.whatsapp.net', '💫 PREFIJO RESTABLECIDO 💫')
 };
 handler.help = ['resetprefix'];
 handler.tags = ['owner'];
