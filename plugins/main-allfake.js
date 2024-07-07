f531629b2756a90a8fd466c25a8adb3336537ac2
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-07 18:23:19 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -89,7 +89,16 @@ global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@
 
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
 
-let icono = ['https://qu.ax/repp.jpg', 'https://qu.ax/ljre.jpg', 'https://qu.ax/vQmu.jpg', 'https://qu.ax/SHbs.jpg', 'https://qu.ax/sVjE.jpg', 'https://qu.ax/STrL.jpg', 'https://qu.ax/AwKr.jpg', 'https://qu.ax/zLSn.jpg'].getRandom() 
+let icono = [
+'https://qu.ax/repp.jpg', 
+'https://qu.ax/ljre.jpg', 
+'https://qu.ax/vQmu.jpg', 
+'https://qu.ax/SHbs.jpg', 
+'https://qu.ax/sVjE.jpg', 
+'https://qu.ax/STrL.jpg', 
+'https://qu.ax/AwKr.jpg', 
+'https://qu.ax/zLSn.jpg'
+].getRandom()
 
 global.rcanal = {
 contextInfo: {
