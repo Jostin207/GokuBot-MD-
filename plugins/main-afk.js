97c03466e650d60d7a9c7d5be252b2305dde1890
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-07 07:30:46 -0500

    ✨◌*̥₊ LᴜғғʏBᴏᴛ-MD ◌❐⚡༉

--- plugins/main-afk.js
+++ plugins/main-afk.js
@@ -1,11 +1,11 @@
 const handler = async (m, {text}) => {
-const user = global.db.data.users[m.sender]
-user.afk = + new Date
-user.afkReason = text
-conn.reply(m.chat, `*📍 El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
-`, m, rcanal)
-}
-handler.help = ['afk [alasan]']
-handler.tags = ['main']
-handler.command = /^afk$/i
-export default handler
+const user = global.db.data.users[m.sender];
+user.afk = + new Date;
+user.afkReason = text;
+conn.reply(m.chat, `🚩 *El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
+`, m, rcanal);
+};
+handler.help = ['afk [alasan]'];
+handler.tags = ['main'];
+handler.command = /^afk$/i;
+export default handler;
