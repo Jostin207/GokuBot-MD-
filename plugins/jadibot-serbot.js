4e6115e9bedea73381dbf945e10be979bd70ae9f
galletita-luffy <yotsuba399@gmail.com>
2024-07-12 17:52:12 -0500

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -131,7 +131,11 @@ conn.ev.off('creds.update', conn.credsUpdate)
 conn.welcome = global.conn.welcome + ''
 conn.bye = global.conn.bye + ''
 conn.spromote = global.conn.spromote + ''
-conn.sdemote = global.conn.sdemote + ''
+conn.sdemote = global.conn.sdemote + '' 
+conn.sDesc = global.conn.sDesc + '' 
+conn.sSubject = global.conn.sSubject + '' 
+conn.sIcon = global.conn.sIcon + '' 
+conn.Revoke = global.conn.sRevoke + '' 
 conn.handler = handler.handler.bind(conn)
 conn.connectionUpdate = connectionUpdate.bind(conn)
 conn.credsUpdate = saveCreds.bind(conn, true)
