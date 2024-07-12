21d4aeed45ccb7505cf522c63fe1df21975dfe4d
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-11 21:44:54 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -7,15 +7,15 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🚩 ¡Hola! Soy Luffy, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
-if (/^que|q$/i.test(m.text)) {
+/* if (/^que|q$/i.test(m.text)) {
 conn.reply(m.chat, `*so* 🧀`, m, rcanal, )
-}
+} */
 if (/^sexo$/i.test(m.text)) {
 conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
 }
-if (/^a$/i.test(m.text)) {
+/* if (/^a$/i.test(m.text)) {
 conn.reply(m.chat, `*rrocito* 🍚`, m, rcanal, )
-}
+} */
 if (/^bug$/i.test(m.text)) {
 conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
 }
