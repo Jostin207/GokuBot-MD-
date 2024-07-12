d4c4c86a55db61255b46013a5257458dc5bb9b5c
galletita-luffy <yotsuba399@gmail.com>
2024-07-12 11:17:20 -0500

    Actualizar fun-pokedex.js

--- plugins/fun-pokedex.js
+++ plugins/fun-pokedex.js
@@ -2,6 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, '🚩 *¿Que Pokémon quieres buscar?.*', m, rcanal)
+await m.react(rwait)
 conn.reply(m.chat, `🍟 *Buscando ${text}*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
@@ -13,8 +14,10 @@ const response = await fetch(url);
 const json = await response.json();
 if (!response.ok) {
 return conn.reply(m.chat, '🍟 *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, rcanal)}
+await m.react(error)
 const luffypokedex = `🚩 *Pokedex - Información de ${json.name}*\n\n🍟 *Nombre:* ${json.name}\n🍟 *ID:* ${json.id}\n🍟 *Tipo:* ${json.type}\n🍟 *Habilidades:* ${json.abilities}\n*Tamaño:* ${json.height}\n🍟 *Peso:* ${json.weight}\n\n📖 *Descripción:*\n${json.description}\n\n🔍 ¡Encuentra más detalles sobre este Pokémon en la Pokedex! 🔍\n\n🔗 https://www.pokemon.com/es/pokedex/${json.name.toLowerCase()}`
-conn.reply(m.chat, luffypokedex, m, rcanal)}
+conn.reply(m.chat, luffypokedex, m, rcanal)
+await m.react(done) }
 
 handler.help = ['pokedex *<pokemon>*']
 handler.tags = ['fun']
