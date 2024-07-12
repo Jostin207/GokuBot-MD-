2b3f3dd40d12576b7d834c287bd2b146d0716e0b
galletita-luffy <yotsuba399@gmail.com>
2024-07-12 18:36:22 -0500

    Actualizar plugins

---
+++ plugins/ai-bard.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+
+var handler = async (m, { text,  usedPrefix, command }) => {
+
+if (!text) return conn.reply(m.chat, `🍟 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces LuffyBot-MD?`, m, rcanal)
+try {
+conn.reply(m.chat, '🚩 Buscando Información...', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+conn.sendPresenceUpdate('composing', m.chat)
+var apii = await fetch(`https://aemt.me/bard?text=${text}`)
+var res = await apii.json()
+await m.reply(res.result)
+} catch (error) {
+console.error(error)
+return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, rcanal)
+}}
+
+handler.command = ['bard']
+handler.help = ['bard']
+handler.tags = ['ai']
+handler.premium = false
+
+export default handler
