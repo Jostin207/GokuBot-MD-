a35c84d43cefa8c27bf1c5144e3627125dc1b1e5
galletita-luffy <yotsuba399@gmail.com>
2024-07-12 11:18:15 -0500

    Actualizar fun-pokedex.js

--- plugins/fun-pokedex.js
+++ plugins/fun-pokedex.js
@@ -13,8 +13,9 @@ const url = `https://some-random-api.com/pokemon/pokedex?pokemon=${encodeURIComp
 const response = await fetch(url);
 const json = await response.json();
 if (!response.ok) {
-return conn.reply(m.chat, '🍟 *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, rcanal)}
-await m.react(error)
+return conn.reply(m.chat, '🍟 *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, rcanal)
+await m.react(error) }
+//await m.react(error)
 const luffypokedex = `🚩 *Pokedex - Información de ${json.name}*\n\n🍟 *Nombre:* ${json.name}\n🍟 *ID:* ${json.id}\n🍟 *Tipo:* ${json.type}\n🍟 *Habilidades:* ${json.abilities}\n*Tamaño:* ${json.height}\n🍟 *Peso:* ${json.weight}\n\n📖 *Descripción:*\n${json.description}\n\n🔍 ¡Encuentra más detalles sobre este Pokémon en la Pokedex! 🔍\n\n🔗 https://www.pokemon.com/es/pokedex/${json.name.toLowerCase()}`
 conn.reply(m.chat, luffypokedex, m, rcanal)
 await m.react(done) }
