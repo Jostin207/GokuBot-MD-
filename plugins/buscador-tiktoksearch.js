0721e78bfc6822fc112bc1e7d4d7891a71de528d
galletita-luffy <yotsuba399@gmail.com>
2024-07-12 20:45:16 -0500

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -13,7 +13,7 @@ usedPrefix: _0x4aa81f,
 command: _0x3f9b74
 }) => {
 if (!_0x2f2134) {
-return conn.reply(m.chat, '🍟 Ingresa El Texto De Lo Que Quieres Buscar En Tiktok', _0x3585f0, rcanal);
+return conn.reply(m.chat, '🍟 Ingresa El Texto De Lo Que Quieres Buscar En Tiktok', _0x3585f0 rcanal);
 }
 async function _0x438e4e(_0x2effca) {
 const {
