40a16d95a6378a2440574649160cea96e64a1b80
galletita-luffy <yotsuba399@gmail.com>
2024-07-12 21:00:42 -0500

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -1,97 +1,90 @@
 import _0x453b96 from 'axios';
 const {
-  proto,
-  generateWAMessageFromContent,
-  prepareWAMessageMedia,
-  generateWAMessageContent,
-  getDevice
+proto,
+generateWAMessageFromContent,
+prepareWAMessageMedia,
+generateWAMessageContent,
+getDevice
 } = (await import("@whiskeysockets/baileys"))["default"];
 let handler = async (_0x3585f0, {
-  conn: _0x1a6b0c,
-  text: _0x2f2134,
-  usedPrefix: _0x4aa81f,
-  command: _0x3f9b74
+conn: _0x1a6b0c,
+text: _0x2f2134,
+usedPrefix: _0x4aa81f,
+command: _0x3f9b74
 }) => {
-  if (!_0x2f2134) {
-    return _0x1a6b0c.reply(_0x3585f0.chat, "🍟 Ingresa El Texto De Lo Que Quieres Buscar En Tiktok", _0x3585f0, rcanal);
-  }
-  async function _0x438e4e(_0x2effca) {
-    const {
-      videoMessage: _0x46952c
-    } = await generateWAMessageContent({
-      'video': {
-        'url': _0x2effca
-      }
-    }, {
-      'upload': _0x1a6b0c.waUploadToServer
-    });
-    return _0x46952c;
-  }
-  function _0x1cb89b(_0x551ce5) {
-    for (let _0xe08334 = _0x551ce5.length - 1; _0xe08334 > 0; _0xe08334--) {
-      const _0x3db8fc = Math.floor(Math.random() * (_0xe08334 + 1));
-      [_0x551ce5[_0xe08334], _0x551ce5[_0x3db8fc]] = [_0x551ce5[_0x3db8fc], _0x551ce5[_0xe08334]];
-    }
-  }
-  try {
-    let _0x26b601 = [];
-    let {
-      data: _0xda4544
-    } = await _0x453b96.get("https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=" + _0x2f2134);
-    let _0x40a9cd = _0xda4544.data;
-    _0x1cb89b(_0x40a9cd);
-    let _0x2d5b59 = _0x40a9cd.splice(0, 7);
-    for (let _0x29b70b of _0x2d5b59) {
-      _0x26b601.push({
-        'body': proto.Message.InteractiveMessage.Body.fromObject({
-          'text': null
-        }),
-        'footer': proto.Message.InteractiveMessage.Footer.fromObject({
-          'text': author
-        }),
-        'header': proto.Message.InteractiveMessage.Header.fromObject({
-          'title': '' + _0x29b70b.title,
-          'hasMediaAttachment': true,
-          'videoMessage': await _0x438e4e(_0x29b70b.nowm)
-        }),
-        'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
-          'buttons': []
-        })
-      });
-    }
-    const _0x33ffca = generateWAMessageFromContent(_0x3585f0.chat, {
-      'viewOnceMessage': {
-        'message': {
-          'messageContextInfo': {
-            'deviceListMetadata': {},
-            'deviceListMetadataVersion': 0x2
-          },
-          'interactiveMessage': proto.Message.InteractiveMessage.fromObject({
-            'body': proto.Message.InteractiveMessage.Body.create({
-              'text': "🍟 Resultado de : " + _0x2f2134
-            }),
-            'footer': proto.Message.InteractiveMessage.Footer.create({
-              'text': "🚩 Tiktok - Search"
-            }),
-            'header': proto.Message.InteractiveMessage.Header.create({
-              'hasMediaAttachment': false
-            }),
-            'carouselMessage': proto.Message.InteractiveMessage.CarouselMessage.fromObject({
-              'cards': [..._0x26b601]
-            })
-          })
-        }
-      }
-    }, {
-      'quoted': _0x3585f0
-    });
-    await _0x1a6b0c.relayMessage(_0x3585f0.chat, _0x33ffca.message, {
-      'messageId': _0x33ffca.key.id
-    });
-  } catch {
-    await _0x3585f0.react(error);
-  }
-};
+if (!_0x2f2134) {
+return _0x1a6b0c.reply(_0x3585f0.chat, "🍟 Ingresa El Texto De Lo Que Quieres Buscar En Tiktok", _0x3585f0, rcanal);
+}
+async function _0x438e4e(_0x2effca) {
+const {
+videoMessage: _0x46952c
+} = await generateWAMessageContent({
+'video': {
+'url': _0x2effca
+}}, {
+'upload': _0x1a6b0c.waUploadToServer
+});
+return _0x46952c;
+}
+function _0x1cb89b(_0x551ce5) {
+for (let _0xe08334 = _0x551ce5.length - 1; _0xe08334 > 0; _0xe08334--) {
+const _0x3db8fc = Math.floor(Math.random() * (_0xe08334 + 1));
+[_0x551ce5[_0xe08334], _0x551ce5[_0x3db8fc]] = [_0x551ce5[_0x3db8fc], _0x551ce5[_0xe08334]];
+}}
+try {
+let _0x26b601 = [];
+let {
+data: _0xda4544
+} = await _0x453b96.get("https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=" + _0x2f2134);
+let _0x40a9cd = _0xda4544.data;
+_0x1cb89b(_0x40a9cd);
+let _0x2d5b59 = _0x40a9cd.splice(0, 7);
+for (let _0x29b70b of _0x2d5b59) {
+_0x26b601.push({
+'body': proto.Message.InteractiveMessage.Body.fromObject({
+'text': null
+}),
+'footer': proto.Message.InteractiveMessage.Footer.fromObject({
+'text': author
+}),
+'header': proto.Message.InteractiveMessage.Header.fromObject({
+ 'title': '' + _0x29b70b.title,
+'hasMediaAttachment': true,
+'videoMessage': await _0x438e4e(_0x29b70b.nowm)
+}),
+'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+'buttons': []
+})});
+}
+const _0x33ffca = generateWAMessageFromContent(_0x3585f0.chat, {
+'viewOnceMessage': {
+'message': {
+'messageContextInfo': {
+'deviceListMetadata': {},
+'deviceListMetadataVersion': 0x2
+},
+'interactiveMessage': proto.Message.InteractiveMessage.fromObject({
+'body': proto.Message.InteractiveMessage.Body.create({
+'text': "🍟 Resultado de : " + _0x2f2134
+}),
+'footer': proto.Message.InteractiveMessage.Footer.create({
+'text': "🚩 Tiktok - Busquedas"
+}),
+'header': proto.Message.InteractiveMessage.Header.create({
+'hasMediaAttachment': false
+}),
+'carouselMessage': proto.Message.InteractiveMessage.CarouselMessage.fromObject({
+'cards': [..._0x26b601]
+})})}}}, {
+'quoted': _0x3585f0
+});
+await _0x1a6b0c.relayMessage(_0x3585f0.chat, _0x33ffca.message, {
+'messageId': _0x33ffca.key.id
+});
+} catch {
+await _0x3585f0.react(error);
+}};
+
 handler.help = ["tiktoksearch <txt>"];
 handler.estrellas = 1;
 handler.tags = ["buscador"];
