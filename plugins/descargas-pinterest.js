924174dc2575aca2a213c7ec4a0e2206cef5e1ab
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-13 08:47:48 -0500

    Actualizar descargas-pinterest.js

--- plugins/descargas-pinterest.js
+++ plugins/descargas-pinterest.js
@@ -14,7 +14,7 @@ let handler = async (_0x10bd40, {
     return _0x9c7141.reply(_0x10bd40.chat, "🍟 *¿Que quieres buscar en pinterest?*", _0x10bd40, rcanal);
   }
     await _0x10bd40.react(rwait);
- _0x9c7141.reply(_0x10bd40.chat, '🚩 *Descargando su imagen de pinterest...*', _0x10bd40, {
+ _0x9c7141.reply(_0x10bd40.chat, '🚩 *Descargando su imagen...*', _0x10bd40, {
  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
  title: packname,
  body: wm,
