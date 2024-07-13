2872a79f52ee0da0da5277acf4b6eb4523025f87
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-12 19:50:45 -0500

    Actualizar anime-imagenes.js

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
-  const res = (await axios.get(`https://raw.githubusercontent.com/OfcDiego/LuffyBot-MD/master/src/JSON/anime-${command}.json`)).data;
-  const haha = await res[Math.floor(res.length * Math.random())];
-  conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m);
-// conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)
+const res = (await axios.get(`https://raw.githubusercontent.com/OfcDiego/LuffyBot-MD/master/src/JSON/anime-${command}.json`)).data;
+const haha = await res[Math.floor(res.length * Math.random())];
+conn.sendFile(m.chat, haha, 'error.jpg', `🍟 *${command}*`, m, null, rcanal);
 };
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
 handler.tags = ['anime'];
