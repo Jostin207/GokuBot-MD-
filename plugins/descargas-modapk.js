0b603d89f30ca5c15def7c9704d529c988b09f5f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-13 09:24:41 -0500

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -19,7 +19,7 @@ let txt = `╭─⬣「 *Aptoide Descargas* 」⬣\n`
     txt += `│  ≡◦ *🕜 Peso ∙* ${data5.size}\n`
     txt += `╰─⬣`
 // let response = `📲 Descargador De Aptoide 📲\n\n🍟 *Nombre:* ${data5.name}\n🚩 *Package:* ${data5.package}\n🎌 *Actualización:* ${data5.lastup}\n📦 *Tamaño:* ${data5.size}`
-await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
+await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, rcanal) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
 return await conn.reply(m.chat, '🛑 *El archivo es demaciado pesado*', m, rcanal )}
