eb2e7742657a7c5cd86ddb18baa5f208a6831cdd
galletita-luffy <yotsuba399@gmail.com>
2024-07-13 09:36:30 -0500

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -1,32 +1,5 @@
 import { search, download } from 'aptoide-scraper'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `*• Ingresa el nombre de la aplicación que deseas descargar.*\n\nEjemplo:\n*${usedPrefix + command}* WhatsApp`, m, rcanal)
-await m.react(rwait)
-try {
-let { name, packname, update, size, thumbnail, dl_url } = await Starlights.aptoide(text)
-if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('El archivo pesa mas de 300 MB, se canceló la Descarga.')}
-let txt = `*乂  A P T O I D E  -  D O W N L O A D*\n\n`
-    txt += `✩ *Nombre* : ${name}\n`
-    txt += `✩ *Package* : ${packname}\n`
-    txt += `✩ *Update* : ${update}\n`
-    txt += `✩ *Peso* :  ${size}\n\n`
-    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lento. . .*`
-await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m, null, rcanal)
-await conn.sendMessage(m.chat, {document: { url: dl_url }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: null }, {quoted: m})
-await m.react(done)
-} catch {
-await m.react(error)
-}}
-handler.help = ['aptoide *<búsqueda>*']
-handler.tags = ['descargas']
-handler.command = ['aptoide', 'apk']
-handler.register = true 
-handler.estrellas = 1
-export default handler
-
-/*import { search, download } from 'aptoide-scraper'
-
 var handler = async (m, {conn, usedPrefix, command, text}) => {
 if (!text) return conn.reply(m.chat, '🚩 *Ingrese el nombre de la apk para descargarlo.*', m, rcanal)
 try {
@@ -39,11 +12,16 @@ previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
-let txt = `📲 Descargador De Aptoide 📲\n\n`
+let txt = `*乂  A P T O I D E  -  D O W N L O A D*\n\n`
+    txt += `✩ *Nombre* : ${data5.name}\n`
+    txt += `✩ *Package* : ${data5.package}\n`
+    txt += `✩ *Update* : ${data5.lastup}\n`
+    txt += `✩ *Peso* :  ${data5.size}`
+/*let txt = `📲 Descargador De Aptoide 📲\n\n`
     txt += `🍟 *Nombre ∙* ${data5.name}\n`
     txt += `🪴 *Package ∙* ${data5.package}\n`
     txt += `⚖ *Peso ∙* ${data5.size}\n`
-    txt += `🚩 *Actualización ∙* ${data5.lastup}`
+    txt += `🚩 *Actualización ∙* ${data5.lastup}`*/
 await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
@@ -58,4 +36,4 @@ handler.command = /^(apkmod|apk|dapk2|aptoide|aptoidedl)$/i
 handler.register = true
 handler.estrellas = 1
 
-export default handler*/
+export default handler
