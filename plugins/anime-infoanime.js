d9f641ba22ede4fbab0c2cec4b5ef6c582ff069b
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-12 19:38:58 -0500

    Actualizar anime-infoanime.js

--- plugins/anime-infoanime.js
+++ plugins/anime-infoanime.js
@@ -22,7 +22,7 @@ let animeingfo = `🍟 Título: ${title_japanese}
 📝 Fondo: ${background}
 💬 Sinopsis: ${synopsis}
  ` 
-conn.sendFile(m.chat, json.data[0].images.jpg.image_url, 'anjime.jpg', '        🚩 *I N F O - A N I M E* 🚩\n' + animeingfo, fkontak, m)
+conn.sendFile(m.chat, json.data[0].images.jpg.image_url, 'anjime.jpg', '      🚩 *I N F O - A N I M E* 🚩\n\n' + animeingfo, fkontak, m)
 
 } 
 handler.help = ['infoanime'] 
