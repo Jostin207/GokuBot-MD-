abe06fee354995a806c4e5b2809d026ba25f6743
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-14 14:33:56 -0500

    Actualizar fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -15,7 +15,7 @@ throw '🚩 *Ocurrió un error*';
 
 handler.help = ['simi', 'bot'];
 handler.tags = ['fun'];
-handler.command = /^((sim)?simi|bot|alexa|cortana)$/i;
+handler.command = /^((sim)?simi|bot|alexa|luffy)$/i;
 export default handler;
 
 async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "es") {
