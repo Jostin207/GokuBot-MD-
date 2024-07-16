6c946b756414dbbc6316a665ec1deb0a61984b20
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 11:47:22 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -379,7 +379,7 @@ plugin.command === command :
 false
 
 if (isAccept) {
-conn.reply(m.chat, `El comando *${command}* no es valido.\nUse #menu para ver mis comandos `, m, rcanal)
+if (isAccept) conn.reply(m.chat, `El comando *${command}* no es valido.\nUse #menu para ver mis comandos `, m, rcanal)
 continue
 } //break
 m.plugin = name
