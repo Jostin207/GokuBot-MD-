2553aabcb4d70617f9ede9a77c7337ace03defff
shrkbadboy <yotsuba399@gmail.com>
2024-07-16 07:49:23 -0500

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,7 +2,7 @@ import yts from 'yt-search'
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
 
-if (!text) return conn.reply(m.chat, `⁖🧡꙰  *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Luffy Bot`, m, rcanal, )
+if (!text) return conn.reply(m.chat, `🍟 *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Luffy Bot`, m, rcanal, )
 
 conn.reply(m.chat, wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
@@ -15,15 +15,19 @@ let results = await yts(text)
 let tes = results.all
 let teks = results.all.map(v => {
 switch (v.type) {
-case 'video': return `⁖❤️꙰༻ *Título:* 
+case 'video': return `🍟 *Título:* 
 » ${v.title}
-⁖🩵꙰༻ *Enlace:* 
+
+🔗 *Enlace:* 
 » ${v.url}
-⁖💜꙰༻ *Duración:*
+
+🕝 *Duración:*
 » ${v.timestamp}
-⁖💚꙰༻ *Subido:* 
+
+🚩 *Subido:* 
 » ${v.ago}
-⁖🧡꙰༻ *Vistas:* 
+
+👀 *Vistas:* 
 » ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
 
 conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
