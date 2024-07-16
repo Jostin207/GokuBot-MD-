cee74b2c36ca52cce64079b00e25a1eb5416924c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 08:03:39 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,9 +2,9 @@ import { spawn } from 'child_process'
 
 var handler = async (m, { conn, isROwner, text }) => {
 
-if (!process.send) throw 'Dont: node rezero.js\nDo: node index.js'
+if (!process.send) throw 'Dont: node luffy.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '⁖💚꙰  *R E I N I C I A N D O* ⁖💚꙰', m, rcanal, )
+await conn.reply(m.chat, '🍟 *R E I N I C I A N D O* 🍟', m, rcanal, )
 process.send('reset')
 } else throw 'eh'
 
