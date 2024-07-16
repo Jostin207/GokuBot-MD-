3462c595cc32402355e52dd2741ee076682fa38a
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 17:16:50 -0500

    Actualizar info-host.js

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -14,13 +14,17 @@ Pues te presento a *Infinity Host*, un hosting que te ofrece una gran variedad d
 - *4GB, 175CPU* = 4dolar
 - *5GB, 200CPU* = 5dolar
 
-🍭 *Dashboard:* https://dash.infinity-wa.com
+🍭 *Dashboard:* 
+• https://dash.infinity-wa.com
 
-🚩 *Panel:* https://panel.infinity-wa.com
+🚩 *Panel:*
+• https://panel.infinity-wa.com
 
-🍧 *Discord:* https://discord.com/invite/vgfpe4Nwd8
+🍧 *Discord:* 
+• https://discord.com/invite/vgfpe4Nwd8
 
-🐢 *Grupo:* https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
+🐢 *Grupo:* 
+• https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
 
 *¿Qué esperas para Adquirir sus servicios? Únete a esta familia :)*`
 await conn.sendMessage(m.chat, { text: txt,
