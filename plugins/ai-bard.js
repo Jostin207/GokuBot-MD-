d66343e7ad2b8cd90777a74507df1425b384690e
shrkbadboy <yotsuba399@gmail.com>
2024-07-16 07:46:53 -0500

    Actualizar ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `🍟 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces LuffyBot-MD?`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🍟 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a LuffyBot-MD?`, m, rcanal)
 try {
 await m.react('⏱️')
 conn.sendPresenceUpdate('composing', m.chat)
