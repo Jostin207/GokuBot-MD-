97cd2820b44bc4785d4974036062412841e18c4b
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 10:37:48 -0500

    Actualizar fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -5,9 +5,9 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, text, command, args, usedPrefix}) => {
 
 if (!text) conn.reply(m.chat, '🍟 Te faltó el texto para hablar con *LuffyBot*', m, rcanal);
-try {
 await m.react(rwait)
 try {
+//await m.react(rwait)
 const resSimi = await simitalk(text);
 conn.sendMessage(m.chat, { text: resSimi.resultado.simsimi }, { quoted: m });
 await m.react(done)
