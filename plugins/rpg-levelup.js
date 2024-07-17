cb7b0ec60cc2b73cb70947e7c158af00e9ca358a
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 22:14:32 -0500

    Actualizar rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -7,10 +7,10 @@ let name = conn.getName(m.sender)
 let user = global.db.data.users[m.sender]
 if (!canLevelUp(user.level, user.exp, global.multiplier)) {
 let { min, xp, max } = xpRange(user.level, global.multiplier)
-let txt = `*Nombre* ${name}\n\n`
-txt += `🚩 Nivel *${user.level}*\n`
-txt += `XP ${user.exp - min} / ${xp}\n\n`
-txt += `No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
+let txt = `🍟 *Nombre* ${name}\n\n`
+txt += `🚩 *Nivel* ${user.level}\n`
+txt += `🍭 *XP* ${user.exp - min} / ${xp}\n\n`
+txt += `🐢 No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
