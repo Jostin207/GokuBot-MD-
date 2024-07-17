8cdd29b4b408b946b3b1a3af7c2dd872042622da
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 22:45:54 -0500

    Actualizar rpg-trabajar.js

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -5,7 +5,7 @@ let user = global.db.data.users[m.sender]
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `🚩 Espera ⏱ *${tiempo2}* para volver a Trabajar.`, m, rcanal)
+conn.reply(m.chat, `🚩 Espera ⏱️ *${tiempo2}* para volver a Trabajar.`, m, rcanal)
 return
 }
 let rsl = Math.floor(Math.random() * 5000)
