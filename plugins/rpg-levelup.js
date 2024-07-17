6ad711a06b83a6484530bd8364241291efcba403
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 21:57:33 -0500

    Actualizar rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -1,4 +1,63 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js'
+let handler = async (m, { conn }) => {
+let name = conn.getName(m.sender)
+let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/1ZxrXKJ/avatar-contact.jpg')
+let user = global.db.data.users[m.sender]
+if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+
+let txt = `*Nombre* ${name}
+
+Nivel *${user.level}* 📊
+XP *${user.exp - min} / ${xp}*
+
+No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
+try {
+let imgg = API('fgmods', '/api/maker/rank', {
+username: name,
+xp: user.exp - min,
+exp: xp,
+avatar: pp,
+level: user.level,
+ranklog: 'https://i.ibb.co/7gfnyMw/gold.png',
+background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
+}, 'apikey')
+
+conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
+} catch (e) {
+m.reply(txt)
+}}
+let before = user.level * 1
+while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+if (before !== user.level) {
+user.role = global.rpg.role(user.level).name
+
+let str = `🎊 F E L I C I T A C I O N E S 🎊 
+
+*${before}* ➔ *${user.level}* [ *${user.role}* ]
+
+• 🧬 Nivel anterior : ${before}
+• 🧬 Nuevos niveles : ${user.level}
+• 📅 Fecha : ${new Date().toLocaleString('id-ID')}
+
+*Nota:* _Cuanto más a menudo interactúes con el bot, mayor será tu nivel_
+`
+try {
+let img = API('fgmods', '/api/maker/levelup', { 
+avatar: pp 
+}, 'apikey')
+conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+} catch (e) {
+m.reply(str)
+}}}
+
+handler.help = ['levelup']
+handler.tags = ['rpg']
+handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
+
+export default handler
+
+/*import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 import can from 'knights-canvas'
 
@@ -54,4 +113,4 @@ handler.help = ['levelup']
 handler.tags = ['rpg']
 handler.command = ['nivel', 'lvl', 'levelup', 'level']
 
-export default handler
+export default handler*/
