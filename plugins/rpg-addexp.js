c1e4ff93d1872ada25d43006fce1e5e672926e80
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 22:52:03 -0500

    Actualizar plugins

---
+++ plugins/rpg-addexp.js
@@ -0,0 +1,32 @@
+import db from '../lib/database.js'
+
+import MessageType from '@whiskeysockets/baileys'
+let impuesto = 0.02
+let handler = async (m, { conn, text }) => {
+let who
+if (m.isGroup) who = m.mentionedJid[0]
+else who = m.chat
+if (!who) throw '🍟 Menciona al usuario con *@user*'
+let txt = text.replace('@' + who.split`@`[0], '').trim()
+if (!txt) throw '🚩 Ingrese la cantidad de *🍭 XP* que quiere transferir.'
+if (isNaN(txt)) throw 'Sólo números.'
+let xp = parseInt(txt)
+let exp = xp
+let imt = Math.ceil(xp * impuesto)
+exp += imt
+if (exp < 1) throw '🚩 Mínimo es 1 🍭 XP.*'
+let users = global.db.data.users
+if (exp > users[m.sender].exp) throw '*🍭 XP* insuficiente para transferir.'
+users[m.sender].exp -= exp
+users[who].exp += xp
+await m.reply(`*${-xp}* 🍭 XP
+Impuesto 2% : *${-imt}* 🍭 XP 
+Total gastado: *${-exp} 🍭 XP*`)
+conn.fakeReply(m.chat, `*+${xp} 🍭 XP.*`, who, m.text)
+}
+handler.help = ['darxp *@user <cantidad>*']
+handler.tags = ['rpg']
+handler.command = ['darxp', 'addexp'] 
+handler.register = true 
+
+export default handler
