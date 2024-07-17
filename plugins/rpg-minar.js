c8f01ce4a3024eba0185a1a7c837e078d1087dc8
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-16 22:28:27 -0500

    Actualizar rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -1,24 +1,19 @@
 let cooldowns = {}
 
 let handler = async (m, { conn }) => {
-
-  let hasil = Math.floor(Math.random() * 5000)
-  let name = conn.getName(m.sender)
-
-  let tiempoEspera = 5 * 60
-  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
-    let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    conn.reply(m.chat, `🚩 Hola ${name}, Ya has minado recientemente, espera ⏱ *${tiempoRestante}* para regresar a la Mina.`, m, rcanal)
-    return
-  }
-
-  global.db.data.users[m.sender].exp += hasil
-  let txt = `🚩 Genial! minaste *${hasil} 💫 XP.*`
-  await m.react('⛏')
-  await conn.reply(m.chat, txt, m, rcanal)
-
-  cooldowns[m.sender] = Date.now()
+let dinero = Math.floor(Math.random() * 5000)
+let tiempo = 5 * 60
+if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
+let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
+conn.reply(m.chat, `🍟 Hola ${nombre}, Ya has minado recientemente, espera ⏱ *${tiempo2}* para regresar a la Mina.`, m, rcanal)
+return
 }
+global.db.data.users[m.sender].exp += hasil
+let minar = `🚩 Genial! minaste *${dinero} 💫 XP.*`
+await m.react('⛏')
+await conn.reply(m.chat, minar, m, rcanal)
+cooldowns[m.sender] = Date.now()}
+
 handler.help = ['minar']
 handler.tags = ['rpg']
 handler.command = ['minar', 'miming', 'mine'] 
@@ -26,8 +21,8 @@ handler.register = true
 export default handler
 
 function segundosAHMS(segundos) {
-  let horas = Math.floor(segundos / 3600)
-  let minutos = Math.floor((segundos % 3600) / 60)
-  let segundosRestantes = segundos % 60
-  return `${minutos} minutos y ${segundosRestantes} segundos`
+let horas = Math.floor(segundos / 3600)
+let minutos = Math.floor((segundos % 3600) / 60)
+let segundosRestantes = segundos % 60
+return `${minutos} minutos y ${segundosRestantes} segundos`
 }
