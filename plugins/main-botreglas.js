3a4ff0e6b7a76626d121225852e2ee8988e9b611
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-23 17:12:33 -0500

    Actualizar main-botreglas.js

--- plugins/main-botreglas.js
+++ plugins/main-botreglas.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let luffy = `🚩 *Respeta las reglas de LuffyBot*\n
+let luffy = `🚩 *Respeta las reglas de GokuBot*\n
 ✰ No llamar Al Bot
 ✰ No hacer spam
 ✰ Contacta al creador si es necesario
@@ -10,10 +10,10 @@ let luffy = `🚩 *Respeta las reglas de LuffyBot*\n
 
 🍬 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas del bot, seras bloqueado.*
 
-⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Luffy.*
+⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Goku.*
 
 ${global.md}`.trim()
-await conn.reply(m.chat, luffy, m, rcanal, )
+await conn.reply(m.chat, goku, m, rcanal, )
 
 }
 handler.help = ['botreglas']
