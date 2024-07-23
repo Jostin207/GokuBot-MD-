2a4b7adf464fb0f6c036d22e4d292cec24a4ecb9
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-23 18:20:25 -0500

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return m.reply('╰⊱🌩⊱ 𝗘𝗥𝗥𝗢𝗥 ⊱🌩⊱╮\n\n🔵 *¡ESTE CHAT NO ESTÁ REGISTRADO!*')
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return m.reply('╰⊱🌩⊱ 𝗘𝗥𝗥𝗢𝗥 ⊱🌩⊱╮\n\n🍬 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*')
+if (!chat.isBanned) return m.reply('╰⊱🌩⊱ 𝗘𝗥𝗥𝗢𝗥 ⊱🌩⊱╮\n\n🍬 *¡EL BOT NO ESTÁ BANEADO EN ESTE CHAT!*')
 chat.isBanned = false
-await conn.reply(m.chat, '╰⊱🌺⊱ 𝗥𝗘𝗔𝗟𝗜𝗭𝗔𝗗𝗢 ⊱🌺⊱╮\n\n🍒 *¡LA BOT YA FUÉ DESBANEADA EN ESTE CHAT!*', m)
+await conn.reply(m.chat, '╰⊱🌺⊱ 𝗥𝗘𝗔𝗟𝗜𝗭𝗔𝗗𝗢 ⊱🌺⊱╮\n\n🍒 *¡EL BOT YA FUÉ DESBANEADO EN ESTE CHAT!*', m)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
-handler.command = /^unbanchat|desbanearchat|desbanchat$/i
+handler.command = /$unbanchatbot|desbanearchatbot|desbanchatbot$/i
 handler.mods = true 
 //handler.group = true
 
