349d06e0be6bfd79acb71802cd0e63635ef75924
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-23 16:14:55 -0500

    ɢᴏᴋᴜʙᴏᴛ-ᴍᴅ 💖

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando LuffyBot ✰');
+console.log('\n✰ Iniciando GokuBot ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Luffy\nBot-MD', {
+say('Goku\nBot-MD', {
 font: 'block',
 align: 'center',
 colors: ['white']
