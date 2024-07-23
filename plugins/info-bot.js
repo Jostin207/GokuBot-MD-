a4f2caf96e8ca32e1399aa9e3377d1e2825f6e10
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-23 16:38:30 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,16 +5,16 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🚩 ¡Hola! Soy Luffy, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `🚩 ¡Hola! Soy Goku, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
 /*if (/^que|q$/i.test(m.text)) {
-conn.reply(m.chat, `*so y arroz* 👌🤌`, m, rcanal, )
+conn.reply(m.chat, `*sorpresa* 👌🤌`, m, rcanal, )
 }*/
 if (/^sexo$/i.test(m.text)) {
 conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
 }
 /*if (/^a$/i.test(m.text)) {
-conn.reply(m.chat, `*rroz y pollo* 😄👌`, m, rcanal, )
+conn.reply(m.chat, `*rroz con pollo* 😄👌`, m, rcanal, )
 }*/
 if (/^bug$/i.test(m.text)) {
 conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
