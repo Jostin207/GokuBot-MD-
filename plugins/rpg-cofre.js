487828fabb609220739e81bde283b9ece68dadd9
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-24 10:10:57 -0500

    Actualizar rpg-cofre.js

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -22,7 +22,7 @@ const handler = async (m, {isPrems, conn}) => {
 ╭━〔 ${global.botname} 〕⬣
 ┃ *${dia} Estrellas* 🌟
 ┃ *${tok} Tokens* 💰
-┃ *${luffy} LuffyCoins* 💸
+┃ *${luffy} GokuCoins* 💸
 ┃ *${expp} Exp* ⚡
 ╰━━━━━━━━━━━━⬣`;
 
