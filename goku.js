8e82e25fca0bfca5c51c4957fa828002d7822eac
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-24 11:52:51 -0500

    Actualizar goku.js

--- goku.js
+++ goku.js
@@ -279,7 +279,7 @@ process.on('uncaughtException', console.error)
 /* Echo por: https://github.com/elrebelde21 */
 
 /*async function connectSubBots() {
-const subBotDirectory = './LuffyJadiBot';
+const subBotDirectory = './GokuJadiBot';
 if (!existsSync(subBotDirectory)) {
 console.log('No se encontraron ningun sub-bots.');
 return;
