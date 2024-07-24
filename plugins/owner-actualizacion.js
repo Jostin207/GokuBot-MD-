d2358d4466478325ce84bfa0d10b0071b8fa05b0
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-24 11:50:47 -0500

    Actualizar owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
-const owner = 'OfcDiego';
-const repo = 'LuffyBot-MD';
+const owner = 'Jostin207';
+const repo = 'GokuBot-MD';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
  conn.sendMessage(m.chat, {text: `*✅️ Comando activado con éxito, te notificaremos cuando se actualize el repositorio.*`}, {quoted: estilo});  
 try {
