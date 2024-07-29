78383ca4eea49899410af0c1da44bd37a8db7a1b
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-28 21:30:14 -0500

    Actualizar info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -6,7 +6,7 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '💌 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, '💌 *Utiliza este comando directamente en el número principal del Bot*', m)
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
@@ -25,14 +25,14 @@ break
 }}}
 
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '💫 *No se encontró ningún archivo que incluya la ID del chat*', m, rcanal, )
+await conn.reply(m.chat, '💫 *No se encontró ningún archivo que incluya la ID del chat*', m)
 } else {
 await conn.reply(m.chat, `✨️ *Se eliminaron ${filesDeleted} archivos de sesión*`, m, fake, )
-conn.reply(m.chat, `💌 *¡Hola! ¿logras verme?*`, m, rcanal, )
+conn.reply(m.chat, `💌 *¡Hola! ¿logras verme?*`, m)
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err)
-await conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, rcanal, )
+await conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m)
 }
 
 }
