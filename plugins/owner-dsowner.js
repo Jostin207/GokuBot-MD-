60acccef6b39ec975e0fecdb3f5fc35da6e7d2bc
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-28 22:01:34 -0500

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -6,9 +6,9 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, '_[🌹] *Utiliza este comando directamente en el número principal del Bot._*', m, rcanal, )
 }
-await conn.reply(m.chat, '🚩 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, rcanal, )
+await conn.reply(m.chat, '_[🌹] *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json..._*', m, rcanal, )
 m.react(rwait)
 
 let sessionPath = './GokuSession/'
@@ -16,7 +16,7 @@ let sessionPath = './GokuSession/'
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, '🚩 *La carpeta está vacía*', m, rcanal, )
+return await conn.reply(m.chat, '_[🌹] *La carpeta está vacía._*', m, rcanal, )
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -27,11 +27,11 @@ filesDeleted++;
 }
 }
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🚩 *La carpeta esta vacía*',  m, rcanal, )
+await conn.reply(m.chat, '_[🌹] *La carpeta esta vacía._*',  m, rcanal, )
 } else {
 m.react(done)
-await conn.reply(m.chat, `🚩 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
-conn.reply(m.chat, `🚩 *¡Hola! ¿logras verme?*`, m, rcanal, )
+await conn.reply(m.chat, `[🌹] *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
+conn.reply(m.chat, `_[🌹] *¡Hola! ¿logras verme?_*`, m, rcanal, )
 
 }
 } catch (err) {
@@ -42,7 +42,7 @@ await conn.reply(m.chat, '🚩 *Ocurrió un fallo*',  m, rcanal, )
 }
 handler.help = ['dsowner']
 handler.tags = ['fix', 'owner']
-handler.command = /^(delzero|dsowner|clearallsession)$/i
+handler.command = /^(delgoku|dsowner|clearallsession)$/i
 
 handler.rowner = true
 
