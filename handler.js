81bf0b6c2d87e15caeb62a9e31847f955509d7c1
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-07-30 14:53:30 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -732,9 +732,9 @@ console.error(e)
 global.dfail = (type, m, conn) => {
 const msg = {
 rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> Jostin207.', 
-owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
+owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.*', 
 mods: '「🤴🏻」 *Esta función solo puede ser usada por mis desarrolladores.*', 
-premium: '「🍧」 *Esta función solo es para usuarios Premium.', 
+premium: '「🍧」 *Esta función solo es para usuarios Premium.*', 
 group: '「🐢」 *Esta funcion solo puede ser ejecutada en grupos.*', 
 private: '「🍭」 *Esta función solo puede ser usada en chat privado.*', 
 admin: '「👑」 *Este comando solo puede ser usado por admins.*', 
