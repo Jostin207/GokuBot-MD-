638d3b6ecae5dd1c4e4acf78725e9acf8e63c5b5
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-29 19:54:52 -0500

    Actualizar plugins

---
+++ plugins/_welcome.js
@@ -0,0 +1,19 @@
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+
+export async function before(m, {conn, participants, groupMetadata}) {
+if (!m.messageStubType || !m.isGroup) return !0
+let vn = 'https://qu.ax/cUYg.mp3'
+let img = imagen1
+let chat = global.db.data.chats[m.chat]
+
+if (chat.welcome && m.messageStubType == 27) {
+this.sendMessage(m.chat, { audio: { url: vn }, contextInfo:{ mentionedJid:[nombre], "externalAdReply": { "thumbnail": img, "title": `乂 Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ 乂`, "body": dev, "previewType": "PHOTO", "thumbnailUrl": null, "showAdAttribution": true,  sourceUrl: [tiktok, yt, md, channel].getRandom()}},  ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak })}
+
+if (chat.welcome && m.messageStubType == 28) {
+this.sendMessage(m.chat, { text: `Se fue @${m.messageStubParameters[0].split`@`[0]} nadie lo va ha extrañar 😹`,
+contextInfo:{ forwardedNewsletterMessageInfo: { newsletterJid: '120363220442272685@newsletter', serverMessageId: '', newsletterName: 'Goku Bot Canal 🫂' }, forwardingScore: 9999999, isForwarded: true, mentionedJid:[nombre], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 Ａ Ｄ Ｉ Ｏ́ Ｓ 乂', body: `Esperemos que no vuelva -_-`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": img, "sourceUrl": `${redes}`}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
+
+if (chat.welcome && m.messageStubType == 32) {
+this.sendMessage(m.chat, { text: `Se fue @${m.messageStubParameters[0].split`@`[0]} nadie lo va ha extrañar 😹`,
+contextInfo:{ forwardedNewsletterMessageInfo: { newsletterJid: '120363220442272685@newsletter', serverMessageId: '', newsletterName: 'Goku Bot Canal. 🫂' }, forwardingScore: 9999999, isForwarded: true, mentionedJid:[nombre], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 Ａ Ｄ Ｉ Ｏ́ Ｓ 乂', body: `Esperemos que no vuelva -_-`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": img, "sourceUrl": `${redes}`}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}}
