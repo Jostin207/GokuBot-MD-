9eb52c532fde9f74d9a44a815a2059ffcfab1673
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <159198564+JOSTINofc@users.noreply.github.com>
2024-08-01 11:59:32 -0500

    Actualizar owner-delprem.js

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -3,21 +3,21 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   const user = global.db.data.users[who];
-  if (!who) throw `🏓 Ingresa un @tag el que quiera quitarle el premium`;
-  if (!user) throw `*☘️ Ese humano no está en mi base de datos*`;
-  if (user.premiumTime = 0) throw '*🥷 El humano no es usuario premium 👑*';
+  if (!who) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝙰𝙻𝙶𝚄𝙽 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝚁 𝙳𝙴 𝙻𝙾𝚂 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`;
+  if (!user) throw `*[❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝚂𝙴 𝙷𝙰 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙻𝙰 𝙱𝙰𝚂𝙴 𝙳𝙴 𝙳𝙰𝚃𝙾𝚂*`;
+  if (user.premiumTime = 0) throw '*[❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝙶𝚁𝙴𝚂𝙰𝙳𝙾 𝙽𝙾 𝙴𝚂 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*';
   const txt = text.replace('@' + who.split`@`[0], '').trim();
 
   user.premiumTime = 0;
 
   user.premium = false;
 
-  const textdelprem = `*@${who.split`@`[0]} ya no es usuario premium 👑*`;
+  const textdelprem = `*[❗] @${who.split`@`[0]} 𝙰𝙷𝙾𝚁𝙰 𝚈𝙰 𝙽𝙾 𝙵𝙾𝚁𝙼𝙰 𝙿𝙰𝚁𝚃𝙴 𝙳𝙴 𝙻𝙾𝚂 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`;
   m.reply(textdelprem, null, {mentions: conn.parseMention(textdelprem)});
 };
 handler.help = ['delprem <@user>'];
 handler.tags = ['owner'];
-handler.command = /^(delprem|remove|-|del)premium$/i;
+handler.command = /^(remove|-|del)prem$/i;
 handler.group = true;
 handler.rowner = true;
 export default handler;
