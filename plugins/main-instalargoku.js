394f8f247fc7fec1ebe1d2c506eda0ceeb569981
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-08-30 11:54:13 -0500

    Actualizar main-instalargoku.js

--- plugins/main-instalargoku.js
+++ plugins/main-instalargoku.js
@@ -13,7 +13,7 @@ apt-get update -y && apt-get upgrade -y
 
 pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn
 
-git clone https://github.com/Jostin207/GokuBot-MD && cd GokuBot-MD && yarn install && npm install 
+git clone https://github.com/Jostin207/GokuBot-MD- && cd GokuBot-MD- && yarn install && npm install 
 
 ls
 
@@ -32,20 +32,11 @@ m.reply('apt-get update -y && apt-get upgrade -y')
 await delay(1000 * 1)
 m.reply('pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn')
 await delay(1000 * 1)
-m.reply('git clone https://github.com/Jostin207/GokuBot-MD && cd GokuBot-MD && yarn install && npm install ')
+m.reply('git clone https://github.com/Jostin207/GokuBot-MD- && cd GokuBot-MD- && yarn install && npm install ')
 await delay(1000 * 1)
 m.reply('ls')
 await delay(1000 * 1)
 m.reply('npm start')
-}
-
-/*if (/^infinity$/i.test(m.text) ) {
-conn.reply(m.chat, '✏️ *Instalación por infinityhost*\n(nosotros no tenemos tutorial pero eso les puede ayudar)\n\n• Dashboard:\nhttps://dashboard.infinitywa.xyz\n\n• Panel:\nhttps://live.panel-infinitywa.store/', m, )
-await delay(2000 * 1)
-conn.sendMessage(m.chat, {image: {url: 'https://telegra.ph/file/64529e19d6c95e97ceab6.jpg'}, caption: ''}, {quoted: fkontak})
-await delay(1000 * 1)
-conn.sendMessage(m.chat, {image: {url: 'https://telegra.ph/file/35384f70f429a9c49468d.jpg'}, caption: ''}, {quoted: fkontak})
-}*/
 }
 
 }
