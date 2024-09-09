93358ee6c2b6613c5ef038add8d7ef72fbf41c72
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-09 11:46:30 -0500

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,43 +1,23 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `🚩 *EQUIPO DE AYUDANTES*
-🌹 *Bot:* ${global.botname}
-✨️  *Versión:* ${global.vs}
+let staff = `🌹 *EQUIPO DE AYUDANTES*
+
+🌱 *Bot:* ${botname}
+🍟 *Versión:* ${vs}
+🪴 *Libreria:* ${libreria + baileys}
 
 👑 *Propietario:*
 
-• 𝖩𝗈𝗌𝗍𝗂𝗇207
-🌹 *Rol:* Propietario
-💖 *Número:* ${creador}
-✨️ *GitHub:* https://github.com/𝖩𝗈𝗌𝗍𝗂𝗇207
+• Jostin207
+🌱 *Rol:* Propietario
+🌴 *Número:* ${creador}
+🚩 *GitHub:* https://github.com/Jostin207
 
-🌻 *Desarrollador:*
+🪴  *Desarrollador:*
 
-• 𝖮𝖿𝖼.𝖣𝗂𝖾𝗀𝗈
-🌹 *Rol:* Desarrollador
-💖 *Número:* Wa.me/573012482597
-✨ *GitHub:* https://github.com/OfcDiego
-
-⁖❤️꙰  *Colaboradores:*
-
-• 𝖬𝖺𝗍𝗂𝖺𝗌-𝖢𝗋𝗒𝗉𝗍𝗈
-🌹 *Rol:* Developer
-💖 *Número:* Wa.me/5492215034412
-✨️ *GitHub:* https://github.com/𝗆𝖺𝗍𝗂𝖺𝗌-𝖼𝗋𝗒𝗉𝗍𝗈
-
-• 𝖤𝖭𝖹𝖮𝖵𝖠𝖲𝖤𝖫𝖤𝖵𝖨𝖢𝖧 (𝖤𝖭𝖹𝖨𝖳𝖮)
-🌹 *Rol:* Developer
-💖 *Número:* Wa.me/5491168758497
-✨️ *GitHub:* https://github.com/𝖤𝗇𝗓𝗈𝖵𝖺𝗌𝖾𝗅𝖾𝗏𝗂𝖼𝗁
-
-• EnderJs-CreatorGL
-🌹 *Rol:* Developer
-💖 *Número:* Wa.me/50558124470
-✨️ *GitHub:* https://github.com/EnderJs-CreatorGL
-
-• Proximamente
-🌹 *Rol:* Developer
-💖 *Número:* PROXIMAMENTE
-✨️ *GitHub:* PROXIMAMENTE`
+• Dev-Diego
+🌱 *Rol:* Desarrollador
+🌴 *Número:* Wa.me/573012482597
+🚩 *GitHub:* https://github.com/Dev-Diego`
 await conn.sendFile(m.chat, icons, 'luffy.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
@@ -46,13 +26,13 @@ externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: false,
 title: `🥷 Developers 👑`,
-body: `🌹 Staff Oficial`,
+body: `🚩 Staff Oficial`,
 mediaType: 1,
 sourceUrl: redes,
-thumbnailUrl: icons
+thumbnailUrl: icono
 }}
 }, { mentions: m.sender })
-m.react(emoji)
+await m.react(emoji)
 
 }
 handler.help = ['staff']
