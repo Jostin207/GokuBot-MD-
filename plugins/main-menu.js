814d3c400be21a0c306cbecbb7cc4367a39d7b50
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-09 12:51:42 -0500

    XD

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -184,7 +184,7 @@ title: null,
 body: null, 
 sourceUrl: redes, 
 thumbnail: icons }}}) 
-await m.react = '✅'
+await m.react = ('✅')
   let category = "imagen"
   const db = './media/database/db.json'
   const db_ = JSON.parse(fs.readFileSync(db))
@@ -198,7 +198,7 @@ await m.react = '✅'
 await conn.sendMini(m.chat, packname, wm, text.trim(), icons, icons, redes, null)
 
   } catch {
-await m.react = '❌'
+await m.react = ('❌')
  conn.reply(m.chat, '❗ Lo sentimos, el menú tiene un error', m, rcanal, )
    // throw e
   }
