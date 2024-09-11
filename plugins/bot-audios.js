22e0d016d9aa54d929a710373cd479f8cde3f14c
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-11 10:26:06 -0500

    Actualizar bot-audios.js

--- plugins/bot-audios.js
+++ plugins/bot-audios.js
@@ -17,7 +17,12 @@ if (/^A Bueno master|Bueno master|Bueno Máster|🫂$/i.test(m.text) && chat.aud
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
 let vn = 'https://qu.ax/xynz.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
-this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: fkontak })}  
+this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: fkontak })} 
+
+if (m.text.match(/(cafirexos|cafipene|superiority|superiorityhost|akirahost|akira|azurahost|azura)/gi)) {    
+let vn = 'https://qu.ax/CQgP.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: fkontak })} 
 
 if (/^corin|corinplus$/i.test(m.text) && chat.audios) {  
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
