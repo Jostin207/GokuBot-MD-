6acf29de5440a0bf5d611847ec5500cf3aa3bf9c
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-12 18:47:27 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -7,23 +7,9 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🚩 ¡Hola! Soy Goku, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
-if (/^sexo$/i.test(m.text)) {
-conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
-}
-if (/^a$/i.test(m.text)) {
-conn.reply(m.chat, `*rroz* 🍚`, m, rcanal, )
-}
-if (/^bug$/i.test(m.text)) {
-conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
 }
 if (/^superiority|superiorityhost$/i.test(m.text)) {
 conn.reply(m.chat, `*Es Un Hosting Bueno* 😄`, m, rcanal, )
-}
-if (/^pene$/i.test(m.text)) {
-conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
-}
-if (/^contexto?|contexto$/i.test(m.text)) {
-conn.reply(m.chat, `*Te La Comes Sin Pretextos* 🤣`, m, rcanal, )
 }
 return !0;
 };
