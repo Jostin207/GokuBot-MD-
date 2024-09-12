195c7062c6a4f9fd1f45bff216c9142a2d89f3e5
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-12 11:05:46 -0500

    Actualizar descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -1,23 +1,29 @@
-/* import Starlights from '@StarlightsTeam/Scraper'
-let listo = '🚩 Tu video de instagram'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingresa un enlace de Instagram.', m, rcanal)
+import { igdl } from "ruhend-scraper"
+
+let handler = async (m, { args, conn }) => { 
+if (!args[0]) {
+return conn.reply(m.chat, '🍟 *Ingresa un link de Instagram*', m, rcanal)}
 try {
-conn.reply(m.chat, '⁖❤️꙰  *Descargando su video de Instagram*', m, {
+await m.react(rwait)
+conn.reply(m.chat, `🕒 *Enviando El Video...*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
 previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-let { dl_url } = await Starlights.igdl(args[0])
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: listo }, { quoted: fkontak})
-} catch (e) {
-  console.log(e)
-  m.reply('🚩 Ocurrió un error inesperado.')
-}}
-handler.help = ['ig <enlace>']
+sourceUrl: channel }}})      
+let res = await igdl(args[0])
+let data = res.data       
+for (let media of data) {
+await new Promise(resolve => setTimeout(resolve, 2000))           
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🍟 *Tu video de instagram.*\n' + textbot, fkontak)
+}} catch {
+await m.react(error)
+conn.reply(m.chat, '🚩 Ocurrió un error.', m, rcanal)}}
+
+handler.command = ['instagram', 'ig']
 handler.tags = ['descargas']
-handler.command = ['ig', 'instagram']
-handler.register = true
+handler.help = ['instagram', 'ig']
 handler.estrellas = 1
-export default handler */
+handler.register = true
+
+export default handler
