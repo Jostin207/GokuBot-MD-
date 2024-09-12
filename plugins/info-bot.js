579c64237bc42e3c8e1337cfc30f22724ccb172a
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-12 17:01:15 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -7,8 +7,11 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🚩 ¡Hola! Soy Goku, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
-if (/^que|q$/i.test(m.text)) {
-conn.reply(m.chat, `*so-rpresa* 🎁`, m, rcanal, )
+if (/^que|q|$/i.test(m.text)) {
+conn.reply(m.chat, `*so* 🧀`, m, rcanal, )
+}
+if (/^so|$/i.test(m.text)) {
+conn.reply(m.chat, `*rpresa* 🗿`, m, rcanal, )
 }
 if (/^sexo$/i.test(m.text)) {
 conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
@@ -19,11 +22,11 @@ conn.reply(m.chat, `*rroz* 🍚`, m, rcanal, )
 if (/^bug$/i.test(m.text)) {
 conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
 }
-if (/^superhost$/i.test(m.text)) {
-conn.reply(m.chat, `*Es Un Hosting* 😄`, m, rcanal, )
+if (/^superiority|superiorityhost$/i.test(m.text)) {
+conn.reply(m.chat, `*Es Un Hosting Bueno* 😄`, m, rcanal, )
 }
-if (/^cu$/i.test(m.text)) {
-conn.reply(m.chat, `*los* 🍑🍑`, m, rcanal, )
+if (/^contexto?|contexto$/i.test(m.text)) {
+conn.reply(m.chat, `*Te La Comes Sin Pretextos* 🤣`, m, rcanal, )
 }
 if (/^pene$/i.test(m.text)) {
 conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
