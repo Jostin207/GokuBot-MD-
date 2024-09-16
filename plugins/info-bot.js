3e309e735e7e043f844254c4125bf4cbc63bee3a
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-16 16:28:32 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -8,8 +8,11 @@ if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🚩 ¡Hola! Soy Goku, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
 }
-if (/^superiority|superiorityhost$/i.test(m.text)) {
-conn.reply(m.chat, `*Es Un Hosting Bueno* 😄`, m, rcanal, )
+if (/^akira|akirahost$/i.test(m.text)) {
+conn.reply(m.chat, `*_Es El Mejor Hosting_* ❤️‍🩹`, m, rcanal, )
+}
+if (/^Goku|gokusito|gokubot-md$/i.test(m.text)) {
+conn.reply(m.chat, `*_Gracias Por Conocerme Bro_* 🫂`, m, rcanal, )
 }
 return !0;
 };
