5e7c7a33e92c2a4d3f829f7da9e83db6fc920840
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-16 20:19:31 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,18 +1,13 @@
-import fs from 'fs';
-const handler = (m) => m;
-handler.all = async function(m) {
+var handler = m => m
+handler.all = async function (m) {
 
-const chat = global.db.data.chats[m.chat];
-if (chat.isBaneed) return
-if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🚩 ¡Hola! Soy Goku, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+let chat = global.db.data.chats[m.chat]
+
+if (/^bot$/i.test(m.text) && !chat.isBanned) {
+    
+m.reply('☁️ *Estoy aquí para ayudarte*', m)}
+
+return !0
+
 }
-if (/^akira|akirahost$/i.test(m.text)) {
-conn.reply(m.chat, `*_Es El Mejor Hosting_* ❤️‍🩹`, m, rcanal, )
-}
-if (/^Goku|gokusito|gokubot-md$/i.test(m.text)) {
-conn.reply(m.chat, `*_Gracias Por Conocerme Bro_* 🫂`, m, rcanal, )
-}
-return !0;
-};
-export default handler;
+export default handler
