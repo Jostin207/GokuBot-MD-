8b5be9d83151a83dd647b651cf2ebb9f881eb195
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-17 11:38:58 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -7,6 +7,10 @@ if (/^bot$/i.test(m.text) && !chat.isBanned) {
     
 m.reply('☁️ *Estoy aquí para ayudarte*', m)}
 
+if (/^akira$/i.test(m.text) && !chat.isBanned) {
+    
+m.reply(' *Es El Mejor Hostin* 🙈🌹', m, rcanal)}
+
 return !0
 
 }
