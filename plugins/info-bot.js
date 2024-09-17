2be054f9add676b6b3d17e2cf16b0ca3dbd3ba01
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-17 11:44:20 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,14 +1,16 @@
-var handler = m => m
-handler.all = async function (m) {
+import fs from 'fs';
+import {sticker} from '../lib/sticker.js';
+const handler = (m) => m;
+handler.all = async function(m) {
 
-let chat = global.db.data.chats[m.chat]
-
-if (/^bot$/i.test(m.text) && !chat.isBanned) {
-    
-m.reply('☁️ *Estoy aquí para ayudarte*', m)}
+const chat = global.db.data.chats[m.chat];
+if (chat.isBaneed) return
+if (/^bot$/i.test(m.text)) {
+conn.reply(m.chat, `✨ ¡Hola! Soy ɢᴏᴋᴜ-ʙᴏᴛ, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+}
 
 if (/^akira|akirahost$/i.test(m.text)) {
-conn.reply(m.chat, `*Es El Mejor Hosting Que Me Patrocina.* 🙈🌹`, m, rcanal, )
+conn.reply(m.chat, `*Es El Mejor Hosting Que Me Patrocina.* 🌹`, m, rcanal, )
 }
 
 return !0
