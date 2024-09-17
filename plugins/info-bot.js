129a3408988e7e8b5b94dc37fdf8de807925c17f
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-17 11:54:40 -0500

    NO CAMBIAR

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -6,11 +6,11 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `✨ ¡Hola! Soy ɢᴏᴋᴜ-ʙᴏᴛ, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `✨ ¡Hola! Soy GokuBot-MD, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
 
 if (/^akira|akirahost$/i.test(m.text)) {
-conn.reply(m.chat, `*_Hola, Ese Hosting Es Muy Bueno y ahí es donde estoy alojado actualmente. Buscas algun servidor con precios bajos? Te presento a Akira Hosting un hosting confiable y de buena calidad, por aqui te dejo la pagina. ( https://dash.akira-host.store ) Gracias por utilizar GokuBot-MD_* 🌹`, m, rcanal, )
+conn.reply(m.chat, `*_Hola, Ese Hosting Es Muy Bueno y ahí es donde estoy alojado actualmente. Buscas algun servidor con precios bajos? Te presento a Akira Host un hosting confiable y de buena calidad, por aqui te dejo la pagina. ( https://dash.akira-host.store ) Gracias por utilizar GokuBot-MD_* 🌹`, m, rcanal, )
 }
 
 return !0
