26131e06b917fcfc4bc0435638e9e132b74262a8
IncognitoOFI <Jostin207oficialmanitoxd@gmail.com>
2024-09-19 17:00:43 -0500

    FF

--- plugins/ff-menu.js
+++ plugins/ff-menu.js
@@ -43,7 +43,8 @@ Significados:
 > » ${usedPrefix}4vs4 22:30 CO
 
 • ${usedPrefix}4vs4 HH:MM CP
-• ${usedPrefix}12vs12 HH:MM CR`.trim();     if (m.isGroup) { 
+• ${usedPrefix}12vs12 HH:MM CR
+• ${usedPrefix}1vs1 HH:MM CO`.trim();     if (m.isGroup) { 
  // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
