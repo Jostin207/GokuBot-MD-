b811ea67ffad87e2a65f71a5db99819aefc01de4
おDiego.xyz <151096440+Dev-Diego@users.noreply.github.com>
2024-09-18 22:04:06 -0500

    Actualizar anime-rollwaifu.js

--- plugins/anime-rollwaifu.js
+++ plugins/anime-rollwaifu.js
@@ -3,13 +3,13 @@ var handler = async (m, { text,  usedPrefix, command }) => {
 // Función para obtener una waifu aleatoria
 
 function obtenerWaifuAleatoria() {
-const imga = JSON.parse(fs.readFileSync(`./src/JSON2/rollwaifu.js`))
+const imga = JSON.parse(fs.readFileSync(`./src/JSON2/rollwaifus.json`))
 const indiceAleatorio = Math.floor(Math.random() * imga.length)
 return imga[indiceAleatorio]}
 
 // Función para mostrar la waifu obtenida
 function mostrarWaifu(waifu) {
-conn.reply(m.chat, `${waifu.name}`, m, rcanal)}
+conn.reply(m.chat, `${name}`, m, rcanal)}
 
 // Ejecuta la función para obtener y mostrar una waifu aleatoria
 const waifuAleatoria = obtenerWaifuAleatoria();
