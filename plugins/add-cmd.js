82ea0c55d582988df4000e7f2744e925aaecfba1
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-07 13:52:58 -0500

    XD

---
+++ plugins/add-cmd.js
@@ -0,0 +1,22 @@
+var handler = async (m, {conn, text, usedPrefix, command}) => {
+
+global.db.data.sticker = global.db.data.sticker || {}
+if (!m.quoted) return conn.reply(m.chat, '*Debes Responder Ah Una Imagen O Sticker*.', m )
+if (!m.quoted.fileSha256) return conn.reply(m.chat, '♨️ *Solo Se Admiten Inagenes O Stickers*.', m )
+if (!text) return conn.reply(m.chat, `🚫 *Error, Te Falto Poner Un texto. Por Ejemplo*:\n${usedPrefix + command} *#menu responder a sticker*`, m )
+
+const sticker = global.db.data.sticker
+const hash = m.quoted.fileSha256.toString('base64')
+
+if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '🚫 *Solo El Creador/Staff Del Bot Pueden Usarlo*.', m )
+sticker[hash] = {text, mentionedJid: m.mentionedJid, creator: m.sender, at: + new Date, locked: false}
+conn.reply(m.chat, `💾 *Comando Guardado*.`, m)
+}
+
+handler.tags = ['database']
+handler.help = ['setcmd']
+handler.command = ['setcmd', 'addcmd', 'cmdadd', 'cmdset']
+
+handler.rowner = true
+
+export default handler
