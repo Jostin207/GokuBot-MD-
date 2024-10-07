f87687485f5cba6d5ebfa202729c1e74ca295285
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-07 14:01:28 -0500

    xD

---
+++ plugins/gc-kick2.js
@@ -0,0 +1,14 @@
+const handler = async (m, {conn, participants, usedPrefix, command}) => {
+  if (!global.db.data.settings[conn.user.jid].restrict) throw '*Las restrinciones no estan activas.*';
+  const kicktext = `Mensiona a la persona para eliminarlo del grupo.`;
+  if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, {mentions: conn.parseMention(kicktext)});
+  if (m.mentionedJid.includes(conn.user.jid)) return;
+  const user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+  const owr = m.chat.split`-`[0];
+  await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+};
+handler.command = /^(kick2|echar2|hechar2|sacar2)$/i;
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+export default handler;
