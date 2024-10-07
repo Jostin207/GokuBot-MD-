e537f10f82262a57500a647bd21c893cb4fbdd14
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-07 14:19:59 -0500

    xD

---
+++ plugins/owner-getplugin.js
@@ -0,0 +1,31 @@
+import cp, {exec as _exec} from 'child_process';
+import {promisify} from 'util';
+import fs from 'fs';
+const exec = promisify(_exec).bind(cp);
+const handler = async (m, {conn, isROwner, usedPrefix, command, text}) => {
+  const ar = Object.keys(plugins);
+  const ar1 = ar.map((v) => v.replace('.js', ''));
+  if (!text) throw `*Ingrese el nombre de algun archivo/plugin que existe en el bot, por ejemplo:*\n\n${usedPrefix + command}* info-estado\n\n*Lista de plugins:*\n*•»* ${ar1.map((v) => ' ' + v).join`\n*•»*`}`;
+  if (!ar1.includes(text)) return m.reply(`*No existe ningun archivo/plugin llamado: "${text}", ingrese uno que exista. por ejemplo:\n\n*•»* ${ar1.map((v) => ' ' + v).join`\n*•»*`}`);
+  let o;
+  try {
+    o = await exec('cat plugins/' + text + '.js');
+  } catch (e) {
+    o = e;
+  } finally {
+    const {stdout, stderr} = o;
+    if (stdout.trim()) {
+      const aa = await conn.sendMessage(m.chat, {text: stdout}, {quoted: m});
+      await conn.sendMessage(m.chat, {document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js`}, {quoted: aa});
+    }
+    if (stderr.trim()) {
+      const aa2 = await conn.sendMessage(m.chat, {text: stderr}, {quoted: m});
+      await conn.sendMessage(m.chat, {document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js`}, {quoted: aa2});
+    }
+  }
+};
+handler.help = ['getplugin'].map((v) => v + ' *<nombre>*');
+handler.tags = ['owner'];
+handler.command = /^(getplugin|getp)$/i;
+handler.rowner = true;
+export default handler;
