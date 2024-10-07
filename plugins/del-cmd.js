78c4b59364ce1fda954e4c3110d4198bd37a3b95
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-07 14:26:40 -0500

    Actualizar del-cmd.js

--- plugins/del-cmd.js
+++ plugins/del-cmd.js
@@ -5,7 +5,7 @@ if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
 if (!hash) return conn.reply(m.chat, `• Ingrese el codigo de asignación, si no sabe cuales son, use este comando. #listcmd`, m, rcanal)
 
 let sticker = global.db.data.sticker
-if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '• Solo el cresdor del bot puede usar esta funcion.', m )
+if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '• Solo el creador del bot puede usar esta funcion.', m )
 delete sticker[hash]
 conn.reply(m.chat, `✅ *Comando Eliminado correctamente*`, m, rcanal)
 
