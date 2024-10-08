849cb338db1d07a391cf11712b7e7e8c80c0a659
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-08 11:05:42 -0500

    Nose

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -31,7 +31,7 @@ ${usedPrefix + command} pconly
 ${usedPrefix + command} gconly
 ${usedPrefix + command} antiprivado`
 
-const isEnable = /true|enable|(turn)?on|1/i.test(command);
+const isEnable = /true|ac|activar|enable|(turn)?on|1/i.test(command);
 const chat = global.db.data.chats[m.chat];
 const user = global.db.data.users[m.sender];
 const bot = global.db.data.settings[conn.user.jid] || {};
