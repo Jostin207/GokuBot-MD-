b873945a3e12c69b1c3a0fdde06ffd24c06bab05
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-08 10:42:48 -0500

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
 sudo dnf clean all -y
 
-RUN git clone https://github.com/Jostin207/GokuBot-MD
+RUN git clone https://github.com/Jostin207/GokuBot-MD-
 
-WORKDIR /root/GokuBot-MD
+WORKDIR /root/GokuBot-MD-
 
-COPY ./root/GokuBot-MD
+COPY ./root/GokuBot-MD-
 
 RUN yarn install
 
