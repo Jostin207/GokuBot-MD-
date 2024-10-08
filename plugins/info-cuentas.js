dfc3882fb5f4ad6da3c21429a31c7e1da913658a
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-10-08 12:04:29 -0500

    Actualizar info-cuentas.js

--- plugins/info-cuentas.js
+++ plugins/info-cuentas.js
@@ -1,19 +1,29 @@
 let media = [imagen1, imagen2, imagen3].getRandom()
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let str = `*[ CUENTAS OFICIALES ]*
+let str = `*CUENTAS OFICIALES DE GOKUBOT-MD*
+
 --------------------
+
 🗃️ *GITHUB*
+
 *${md}*
+
 --------------------
+
 💌 *INSTAGRAM*
+
 *${ig}*
+
 --------------------
+
 🚀 *YOUTUBE*
+
 *${yt}*
+
 --------------------`
 
-await conn.sendFile(m.chat, media, 'Menu1.jpg', str, m, rcanal)}
+await conn.sendFile(m.chat, media, 'avatar_contact.png', m, rcanal)}
 
 handler.tags = ['main']
 handler.command = /^cuentasoficiales|cuentasofc|cuentas$/i
